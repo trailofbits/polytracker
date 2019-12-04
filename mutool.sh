b6fed2235bf2db8ecb6ccd91af9c50e1fa8885f5
Brad Larsen <brad.larsen@trailofbits.com>
2019-12-04 17:53:12 -0500

    Add missing quotes in mutool.sh script
    Fixes #3.

--- mutool.sh
+++ mutool.sh
@@ -7,4 +7,4 @@ if [[ "$(docker images -q trailofbits/tappdemo 2> /dev/null)" == "" ]]; then
     docker build -t trailofbits/tappdemo -f Dockerfile.demo .
 fi
 
-docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/tappdemo:latest /polytracker/the_klondike/mupdf/bin/bin/mutool draw $1
+docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/tappdemo:latest /polytracker/the_klondike/mupdf/bin/bin/mutool draw "$1"
