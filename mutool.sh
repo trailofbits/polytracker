4e1d96bc15264c824ac8697a9a4105df989353ff
Brad Larsen <brad.larsen@trailofbits.com>
2019-12-12 14:10:33 -0500

    Update mupdf wrapper scripts after merge of PR #28

--- mutool.sh
+++ mutool.sh
@@ -3,8 +3,8 @@
 if [[ "$(docker images -q trailofbits/polytracker 2> /dev/null)" == "" ]]; then
     docker build -t trailofbits/polytracker .
 fi
-if [[ "$(docker images -q trailofbits/polytracker-demo 2> /dev/null)" == "" ]]; then
-    docker build -t trailofbits/polytracker-demo -f Dockerfile.demo .
+if [[ "$(docker images -q trailofbits/polytracker-demo-mupdf 2> /dev/null)" == "" ]]; then
+    docker build -t trailofbits/polytracker-demo-mupdf -f Dockerfile-mupdf.demo .
 fi
 
-docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo:latest /polytracker/the_klondike/mupdf/bin/bin/mutool draw "$1"
+docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo-mupdf:latest mutool draw "$1"
