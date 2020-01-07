f207baf8194637208c35a1da47431308a16584eb
Carson Harmon <carson.harmon@trailofbits.com>
2020-01-07 15:15:10 -0500

    Added logging to be after loads and stores, more slowdown, but logging

--- polytracker/dfsan/dfsan_pass/DataFlowSanitizer.cpp
+++ polytracker/dfsan/dfsan_pass/DataFlowSanitizer.cpp
@@ -1401,6 +1401,7 @@ void DFSanVisitor::visitLoadInst(LoadInst &LI) {
     DFSF.NonZeroChecks.push_back(Shadow);
 
   DFSF.setShadow(&LI, Shadow);
+  logTaintedOps(&LI, Shadow);
 }
 
 void DFSanFunction::storeShadow(Value *Addr, uint64_t Size, uint64_t Align,
@@ -1476,6 +1477,7 @@ void DFSanVisitor::visitStoreInst(StoreInst &SI) {
     Shadow = DFSF.combineShadows(Shadow, PtrShadow, &SI);
   }
   DFSF.storeShadow(SI.getPointerOperand(), Size, Align, Shadow, &SI);
+  logTaintedOps(&SI, Shadow);
 }
 
 void DFSanVisitor::visitBinaryOperator(BinaryOperator &BO) {
