b96ff78a7ca23521472e1c16947bae93f928ca97
Carson Harmon <carson.harmon@trailofbits.com>
2020-01-21 18:01:11 -0500

    Refactor to use taintPropagationMangaer - segfault in post processing, :thinking_emoji:

--- polytracker/dfsan/dfsan_includes/dfsan_types.h
+++ polytracker/dfsan/dfsan_includes/dfsan_types.h
@@ -1,12 +1,13 @@
 #ifndef DFSAN_TYPES_H
 #define DFSAN_TYPES_H
 #include "dfsan_rt/sanitizer_common/sanitizer_internal_defs.h" 
+#include "dfsan_rt/sanitizer_common/sanitizer_atomic.h" 
 #include <stdint.h> 
 
 using __sanitizer::uptr;
 using __sanitizer::u16;
 using __sanitizer::u32;
-
+using namespace __dfsan; 
 /* This defines how many bits are used for dfsan labels.
  * It automatically influences the types of `dfsan_label`,
  * `atomic_dfsan_label`, `uint_dfsan_label_t`, as well as
@@ -20,6 +21,10 @@ using __sanitizer::u32;
 
 // Copy declarations from public sanitizer/dfsan_interface.h header here.
 typedef PPCAT(u, DFSAN_LABEL_BITS) dfsan_label;
+
+typedef PPCAT(PPCAT(atomic_uint, DFSAN_LABEL_BITS), _t) atomic_dfsan_label;
+// An unsigned int big enough to address a dfsan label:
+typedef PPCAT(PPCAT(uint, DFSAN_LABEL_BITS), _t) uint_dfsan_label_t;
 
 typedef uint32_t decay_val;
 #undef PPCAT_NX
--- polytracker/dfsan/dfsan_rt/dfsan/dfsan_log_mgmt.h
+++ polytracker/dfsan/dfsan_rt/dfsan/dfsan_log_mgmt.h
@@ -1,59 +1,59 @@
-#ifndef DFSAN_TAINT_INFO
-#define DFSAN_TAINT_INFO
+#ifndef DFSAN_LOG_TAINT
+#define DFSAN_LOG_TAINT
+
+#include "../sanitizer_common/sanitizer_atomic.h"
+#include "../sanitizer_common/sanitizer_common.h"
+#include "../sanitizer_common/sanitizer_file.h"
+#include "../sanitizer_common/sanitizer_flags.h"
+#include "../sanitizer_common/sanitizer_flag_parser.h"
+#include "../sanitizer_common/sanitizer_libc.h"
 
 #include <vector>
 #include <string>
 #include <unordered_map> 
 #include <unordered_set> 
 #include <mutex>
+#include <iostream> 
 
 #include "lrucache/lrucache.hpp"
 #include "dfsan/dfsan.h"
 //Amalgamated CRoaring files
-#include "roaring.hh"
-#include "roaring.c"
 #include "polytracker.h"
 #include "json.hpp"
 
-//FIXME assign actual defaults 
-#define DEFAULT_TTL 10
-#define DEFAULT_CACHE_SIZE 1000 
-
-//NOTE intervals are depricated for now 
-#define DEFAULT_INTERVAL 0 
+using json = nlohmann::json; 
 
 typedef std::unordered_map<std::thread::id, std::vector<std::string>> thread_id_map; 
 typedef std::unordered_map<std::string, std::unordered_set<taint_node_t*>> string_node_map; 
 
 /*
- * This class is responsible for dumping to json at specific time intervals 
- * Currently depricated 
+ * This manages the mapping between taint_label <--> taint_node 
+ * NOTE an instance of this class is shared between the log manager and prop manager 
+ * It has its own lock to prevent concurency issues 
  */
-class taintIntervalManager {
-	public: 
-		taintIntervalManager(); 
-
+class taintMappingManager {
+	public:
+		taintMappingManager(char * shad_mem_ptr, char * taint_forest_ptr);
+		~taintMappingManager(); 
+		inline taint_node_t * getTaintNode(dfsan_label label); 
+		inline dfsan_label getTaintLabel(taint_node_t * node);
 	private: 
-		clock_t prev_time; 
-		clock_t curr_time; 
-		double interval_time; 
-};
+		std::mutex taint_mapping_lock;
+	 	char * shad_mem; 
+		char * forest_mem; 	
+}; 
 
 /*
  * This class is responsible for converting vectors to output (json or raw) 
  */
 class taintOutputManager {
 	public:
-		taintOutputManager(char * outfile, bool dump_raw_taint_info); 
-		taintOutputManager(bool dump_raw_taint_info); 
-		taintOutputManager(char * outfile); 
-		taintOutputManager(); 
-		void setOutputFile(char * outfile);
-		void setDumpRaw(bool dump_raw);
+		taintOutputManager(char * outfile, bool dump_raw_taint_info, taintMappingManager * map_mgr); 
 	private:
 		std::string outfile; 
 		bool dump_raw_taint_info;
-	 	json output_json; 	
+	 	json output_json;
+		taintMappingManager * map_manager; 	
 };
 
 /*
@@ -61,9 +61,9 @@ class taintOutputManager {
  */
 class taintLogManager {
 	public:
-		taintInfoManager(char * outfile); 
-		taintInfoManager(); 
-		taintInfoManager~(); 
+		taintLogManager(char * outfile, taintMappingManager * map_mgr); 
+		taintLogManager(); 
+		~taintLogManager(); 
 		void logCompare(dfsan_label some_label); 
 		void logOperation(dfsan_label some_label);
 		void logFunctionEntry(std::string fname); 
@@ -73,10 +73,8 @@ class taintLogManager {
 		string_node_map function_to_bytes;
 	 	string_node_map function_to_cmp_bytes;
 		std::unordered_map<std::string, std::unordered_set<std::string>> runtime_cfg;
-
 		std::mutex taint_log_lock;
 		taintOutputManager * output_manager; 
-		taintIntervalManager * interval_manager; 
 };
 
 /*
@@ -84,17 +82,22 @@ class taintLogManager {
  */
 class taintPropagationManager {
 	public: 
-		taintPropagationManager(char * shad_mem_ptr, char * taint_forest_ptr); 
-		taintPropagationManager~();
+		taintPropagationManager(taintMappingManager * map_mgr, decay_val init_decay_val, dfsan_label start_union_label); 
+		~taintPropagationManager();
 	 	dfsan_label createNewLabel(dfsan_label offset, taint_source_id taint_id); 
-		dfsan_label createUnionLabel(dfsan_label l1, dfsan_label l2); 	
+	 	dfsan_label unionLabels(dfsan_label l1, dfsan_label l2); 	
 	private:
-	 	std::unordered_map<dfsan_label, std::unordered_map<dfsan_label, dfsan_label>> union_table; 	
+		void _checkMaxLabel(dfsan_label label); 
+		dfsan_label _createUnionLabel(dfsan_label l1, dfsan_label l2, decay_val init_decay);
+	 	//This is a data structures that helps prevents repeat pairs of bytes from generating new labels.
+		//The original in DFsan was a matrix that was pretty sparse, so this saves space and also helps
+		//While its not a full solution, it actually reduces the amount of labels by a lot, so its worth having
+		std::unordered_map<dfsan_label, std::unordered_map<dfsan_label, dfsan_label>> union_table; 	
 		decay_val taint_node_ttl;
 		std::mutex taint_prop_lock;
-		char * shadow_mem;
-		char * taint_forest; 	
-
+	  //Using atomic store and load to access this, so if we change our locking later its still fine	
+		atomic_dfsan_label next_union_label; 	
+		taintMappingManager * map_manager; 
 };
 
 #endif 
