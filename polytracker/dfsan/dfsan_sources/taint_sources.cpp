61faa9cf78c07cb8a37396437ecd45c6be256517
Carson Harmon <carson.harmon@trailofbits.com>
2020-02-05 14:28:34 -0500

    Add support for pread64

--- polytracker/dfsan/dfsan_sources/taint_sources.cpp
+++ polytracker/dfsan/dfsan_sources/taint_sources.cpp
@@ -178,6 +178,44 @@ __dfsw_pread(int fd, void *buf, size_t count, off_t offset,
 	return ret;
 }
 
+EXT_C_FUNC ssize_t
+__dfsw_pread64(int fd, void *buf, size_t count, off_t offset,
+		dfsan_label fd_label, dfsan_label buf_label,
+		dfsan_label count_label, dfsan_label offset_label,
+		dfsan_label *ret_label) {
+	//FIXME 
+	std::cout << "Inside of pread64" << std::endl; 
+	ssize_t ret = pread(fd, buf, count, offset);
+	if (taint_info_manager->isTracking(fd)) {
+		if (ret > 0) {
+			taint_info_manager->taintData(fd, (char*)buf, offset, ret);
+		}
+		*ret_label = 0;  
+	} else {
+		*ret_label = 0;
+	}
+	return ret;
+}
+
+EXT_C_FUNC ssize_t
+__dfsw___libc_pread64(int fd, void *buf, size_t count, off_t offset,
+		dfsan_label fd_label, dfsan_label buf_label,
+		dfsan_label count_label, dfsan_label offset_label,
+		dfsan_label *ret_label) {
+	//FIXME 
+	std::cout << "Inside of __libc_pread64" << std::endl; 
+	ssize_t ret = pread(fd, buf, count, offset);
+	if (taint_info_manager->isTracking(fd)) {
+		if (ret > 0) {
+			taint_info_manager->taintData(fd, (char*)buf, offset, ret);
+		}
+		*ret_label = 0;  
+	} else {
+		*ret_label = 0;
+	}
+	return ret;
+}
+
 EXT_C_FUNC size_t
 __dfsw_fread(void *buff, size_t size, size_t count, FILE *fd,
 		dfsan_label buf_label, dfsan_label size_label,
