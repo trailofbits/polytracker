4265a5f116547b92024e141300a080cf4e0054c9
Carson Harmon <carson.harmon@trailofbits.com>
2020-06-03 20:05:17 -0400

    Update taint sources

--- polytracker/src/dfsan_sources/taint_sources.cpp
+++ polytracker/src/dfsan_sources/taint_sources.cpp
@@ -21,7 +21,7 @@
 #define BYTE 1
 #define EXT_C_FUNC extern "C" __attribute__((visibility("default")))
 #define EXT_CXX_FUNC extern __attribute__((visibility("default")))
-
+#define DEBUG_INFO 
 #define PPCAT_NX(A, B) A##B
 #define PPCAT(A, B) PPCAT_NX(A, B)
 #ifdef DEBUG_INFO
