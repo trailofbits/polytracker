7206ce43706002fc347d8c11b104bba4aaa9caa6
Carson Harmon <carson.harmon@trailofbits.com>
2020-06-10 18:42:24 -0400

    Add gclang build to polybuild

--- polybuild/polybuild.py
+++ polybuild/polybuild.py
@@ -36,15 +36,17 @@ class CompilerMeta:
 
 
 class PolyBuild:
-    def __init__(self):
-        self.meta = CompilerMeta(self.poly_check_cxx(sys.argv[0]),
+    def __init__(self, argv):
+        self.meta = CompilerMeta(self.poly_check_cxx(argv[0]),
                                  self.poly_find_dir(os.path.realpath(__file__)) + "/")
+        self.argv = argv
 
     def poly_check_cxx(self, compiler: str) -> bool:
         """
         Checks if compiling a c++ or c program
         """
-        if compiler.find("++"):
+        print(compiler)
+        if compiler.find("++") != -1:
             return True
         return False
 
@@ -57,6 +59,13 @@ class PolyBuild:
         if last_slash == -1:
             return "."
         return compiler_path[0:last_slash]
+
+    def poly_is_linking(self, argv) -> bool:
+        nonlinking_options = ["-E", "-fsyntax-only", "-S", "-c"]
+        for option in argv:
+            if option in nonlinking_options:
+                return False
+        return True
 
     def poly_add_inst_lists(self, directory: str) -> Optional[List[str]]:
         """
@@ -144,6 +153,35 @@ class PolyBuild:
             return False
         return True
 
+    def poly_build(self) -> bool:
+        compile_command = []
+        if self.meta.is_cxx:
+            compile_command.append("gclang++")
+        else:
+            compile_command.append("gclang")
+        compile_command.append("-pie -fPIC")
+        if self.meta.is_cxx:
+            compile_command.append("-stdlib=libc++")
+            compile_command.append("-nostdinc++")
+            compile_command.append("-I" + self.meta.compiler_dir + "/../cxx_libs/include/c++/v1/")
+            compile_command.append("-L" + self.meta.compiler_dir + "/../cxx_libs/lib/")
+        for arg in self.argv[1:]:
+            compile_command.append(arg)
+        is_linking = self.poly_is_linking(self.argv)
+        if is_linking:
+            # If its cxx, link in our c++ libs
+            if self.meta.is_cxx:
+                compile_command.append("-lc++ -lc++abipoly -lc++abi")
+            # Else, we still need stdc++ for our runtime
+            else:
+                compile_command.append("-lstdc++")
+            compile_command.append("-lpthread -ldl -lrt -lm")
+        print(" ".join(compile_command))
+        res = os.system(" ".join(compile_command))
+        if res != 0:
+            return False
+        return True
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -172,293 +210,33 @@ def main():
                                                                          "with instrumentation")
     parser.add_argument("--libs", nargs='+', default=[], help="Specify libraries to link with the instrumented target"
                                                               "--libs -llib1 -llib2 -llib3 etc")
+    poly_build = PolyBuild(sys.argv)
 
-    args = parser.parse_args(sys.argv[1:])
-
-    poly_build = PolyBuild()
-    # Do polyOpt/Compile
-    if args.instrument:
-        if args.output_file is None:
-            print("Error! Outfile not specified, please specify with -o")
-            sys.exit(1)
-        if args.input_file is None:
-            print("Error! Input file not specified, please specify with -f")
-            sys.exit(1)
-        if not os.path.exists(args.input_file):
-            print("Error! Input file could not be found!")
-            sys.exit(1)
-        res = poly_build.poly_instrument(args.input_file, args.output_file, args.output_bitcode_file, args.libs)
-        if not res:
-            sys.exit(1)
-    # do Build and opt/Compile
-    elif args.target_instrument:
-        pass
+    if sys.argv[1] == "--instrument" or sys.argv[1] == "--target-instrument":
+        args = parser.parse_args(sys.argv[1:])
+        # Do polyOpt/Compile
+        if args.instrument:
+            if args.output_file is None:
+                print("Error! Outfile not specified, please specify with -o")
+                sys.exit(1)
+            if args.input_file is None:
+                print("Error! Input file not specified, please specify with -f")
+                sys.exit(1)
+            if not os.path.exists(args.input_file):
+                print("Error! Input file could not be found!")
+                sys.exit(1)
+            res = poly_build.poly_instrument(args.input_file, args.output_file, args.output_bitcode_file, args.libs)
+            if not res:
+                sys.exit(1)
+        # do Build and opt/Compile
+        elif args.target_instrument:
+            pass
     # Do gllvm build
     else:
-        pass
+        res = poly_build.poly_build()
+        if not res:
+            sys.exit(1)
 
 
 if __name__ == "__main__":
     main()
-"""
-//Does OPT instrumentation then does the lowering into an executable.
-//polyclang --instrument -f file.bc -o exec -lwhatever1 -lwhatever2...
-static int PolyInstrument(int old_argc, char *old_argv[], int &new_argc,
-                              vector<std::string> &new_argv) {
-    std::vector<std::string> linked_libraries;
-//Collect args for output
-std::string output_filepath = "";
-std::string input_filepath = "";
-
-//This is to store the temporary build artifact before we produce the bin
-                                                                      //For debugging this can be overriden by specifying --output-bitcode
-std::string temp_bitcode_filepath = "/tmp/temp_bitcode.bc";
-for (int i = 2; i < old_argc; i++) {
-    std::string curr_arg = std::string(old_argv[i]);
-//Grab the output file name
-std::cout << "CURR ARG IS " << curr_arg << std::endl;
-std::cout << "IS F? " << curr_arg.compare("-f") << std::endl;
-if (curr_arg.compare("-o") == 0) {
-    std::cout << "Setting output?" << std::endl;
-if (i + 1 >= old_argc) {
-    std::cout << "Error! No output file specified" << std::endl;
-return -1;
-}
-output_filepath = std::string(old_argv[i + 1]);
-std::cout << "Output is: " << output_filepath << std::endl;
-i++;
-}
-//Check if its a library
-else if (curr_arg.find("-l") == 0) {
-linked_libraries.push_back(curr_arg);
-}
-else if (curr_arg.compare("--output-bitcode") == 0) {
-if (i + 1 >= old_argc) {
-std::cout << "Error! Tried to find output-bitcode filepath" << std::endl;
-return -1;
-}
-temp_bitcode_filepath = old_argv[i + 1];
-i++;
-}
-else if (curr_arg.compare("-f") == 0) {
-std::cout << "Setting file?" << std::endl;
-if (i + 1 >= old_argc) {
-std::cout << "Error! Tried to find input bitcode file from -f" << std::endl;
-return -1;
-}
-std::cout << "SETTING INPUT FILE PATH" << std::endl;
-input_filepath = old_argv[i + 1];
-i++;
-}
-else {
-    std::cout << "Error! Unknown argument!" << std::endl;
-return -1;
-}
-}
-if (input_filepath.empty()) {
-std::cout << "Error! Input file path not specified, please specify with -f" << std::endl;
-return -1;
-}
-if (output_filepath.empty()) {
-std::cout << "Error! Output file path not specified, please specify with -o" << std::endl;
-}
-std::cout << "Input file path is: " << input_filepath << std::endl;
-//Sanity check that all file paths can be accessed
-int fd = open(input_filepath.c_str(), O_RDONLY);
-if (fd < 0) {
-std::cout << "Error! Unable to input filepath open: " << input_filepath << std::endl;
-return -1;
-}
-close(fd);
-//Instrument the bitcode and confirm temp bitcode created
-int res = PolyOpt(input_filepath, temp_bitcode_filepath);
-if (res < 0) {
-return -1;
-}
-std::cout << "Bitcode is " << temp_bitcode_filepath << std::endl;
-std::cout << "Output is: " << output_filepath << std::endl;
-//Link the bitcode with the specified libs if any, and our instrumentation libs
-res = PolyCompile(temp_bitcode_filepath, output_filepath, linked_libraries);
-if (res < 0) {
-return -1;
-}
-return 0;
-}
-
-
-static void PolyBuild(int old_argc, char *old_argv[], int &new_argc,
-vector<std::string> &new_argv) {
-
-if (compiler_meta.is_cxx) {
-new_argv.push_back("gclang++");
-} else {
-new_argv.push_back("gclang");
-}
-/*
-new_argv.push_back("-Xclang");
-new_argv.push_back("-load");
-new_argv.push_back("-Xclang");
-new_argv.push_back(compiler_meta.compiler_dir +
-                   "/pass/libDataFlowSanitizerPass.so");
-new_argv.push_back("-mllvm");
-new_argv.push_back("-polytrack-dfsan-abilist=" + compiler_meta.compiler_dir +
-                   "/abi_lists/polytrack_abilist.txt");
-new_argv.push_back("-mllvm");
-new_argv.push_back("-polytrack-dfsan-abilist=" + compiler_meta.compiler_dir +
-                   "/abi_lists/dfsan_abilist.txt");
-*/
-new_argv.push_back("-pie");
-new_argv.push_back("-fPIC");
-//TODO Optimize flags
-
-if (compiler_meta.is_cxx && compiler_meta.is_libcxx == false) {
-new_argv.push_back("-stdlib=libc++");
-new_argv.push_back("-nostdinc++");
-new_argv.push_back("-I" + compiler_meta.compiler_dir + "cxx_libs/include/c++/v1/");
-new_argv.push_back("-L" + compiler_meta.compiler_dir + "cxx_libs/lib/");
-}
-// Push back the rest of args to clang
-// Here we catch some args that clang 7.1 does not support,
-// That build systems like PDFium use
-for (int i = 1; i < old_argc; i++) {
-    std::string curr_string = old_argv[i];
-new_argv.push_back(curr_string);
-}
-/*
-for (int i = 1; i < old_argc; i++) {
-    std::string curr_string = old_argv[i];
-if (curr_string.find("-ftrivial-auto-var-init") != std::string::npos ||
-curr_string.find("-fintegrated-cc1") != std::string::npos ||
-                                                     curr_string.find("-debug-info-kind=constructor") != std::string::npos ||
-                                                                                                                      curr_string.find("-gsplit-dwarf") != std::string::npos) {
-    #ifdef DEBUG_INFO
-    printf("Skipping!\n");
-#endif
-continue;
-}
-new_argv.push_back(curr_string);
-}
-*/
-
-// The last args to the compiler should be for linking
-                                               // Here we bundle our own required libs as a "group" to prevent circ
-// dependencies
-if (compiler_meta.is_linking) {
-/*
-new_argv.push_back("-Wl,--start-group");
-new_argv.push_back("-lpthread");
-new_argv.push_back("-ldl");
-new_argv.push_back("-lrt");
-new_argv.push_back("-lm");
-*/
-/*
-* While there are usually implicitly linked, complex build systems
-* might specify -nostdlibs -nostdinc++, we just link them manually
-*/
-if (compiler_meta.is_cxx && compiler_meta.is_libcxx == false) {
-new_argv.push_back("-lc++");
-new_argv.push_back("-lc++abipoly");
-new_argv.push_back("-lc++abi");
-}
-if (compiler_meta.is_cxx == false) {
-new_argv.push_back("-lstdc++");
-}
-new_argv.push_back("-lpthread");
-new_argv.push_back("-ldl");
-new_argv.push_back("-lrt");
-new_argv.push_back("-lm");
-//new_argv.push_back("-lgcc_s");
-//new_argv.push_back("-lc");
-//if (compiler_meta.is_cxx) {
-//	new_argv.push_back("-lstdc++");
-//}
-// Force the linker to include all of our instrumentation
-                                          /*
-                                          new_argv.push_back("-Wl,--whole-archive");
-new_argv.push_back(compiler_meta.compiler_dir +
-"/lib/libdfsan_rt-x86_64.a");
-new_argv.push_back("-Wl,--no-whole-archive");
-*/
-// Tell the linker what we should export (important, instrumented code needs
-// the symbols)
-/*
-new_argv.push_back("-Wl,--dynamic-list=" + compiler_meta.compiler_dir +
-"/lib/libdfsan_rt-x86_64.a.syms");
-
-// Link in our custom function wrappers that act as taint sources
-new_argv.push_back(compiler_meta.compiler_dir + "/lib/libTaintSources.a");
-
-// This is the "private" libcxx for dfsan, its uninstrumented
-                                               //Carson - We should not need this if we are actually linking properly
-new_argv.push_back(compiler_meta.compiler_dir + "/lib/libc++.a");
-new_argv.push_back(compiler_meta.compiler_dir + "/lib/libc++abi.a");
-*/
-//new_argv.push_back("-Wl,--end-group");
-// You need to compile with -pie -fPIC, otherwise the sanitizer stuff wont
-// work This is because the sanitizer creates the area and arranges everything
-// in its own way If its not PIE, you cant move it, so you'll segfault on
-// load. Hard to debug, just keep this
-}
-new_argc = new_argv.size();
-}
-
-int main(int argc, char *argv[]) {
-
-#ifdef DEBUG_INFO
-fprintf(stderr, "===ORIGINAL ARGS===\n");
-for (int i = 0; i < argc; i++) {
-fprintf(stderr, "%s\n", argv[i]);
-}
-fprintf(stderr, "===END ORIGINAL ARGS===\n");
-#endif
-compiler_meta.is_cxx = PolyCheckCxx(argv[0]);
-compiler_meta.is_linking = PolyCheckLinking(argc, argv);
-compiler_meta.compiler_dir = PolyFindDir(argv[0]);
-char * is_libcxx = getenv("POLYCXX");
-if (is_libcxx != NULL) {
-compiler_meta.is_libcxx = true;
-}
-else {
-compiler_meta.is_libcxx = false;
-}
-// This is hard coded because to build some targets they expect things to be
-// in certain places By hardcoding this path we can always find where our libs
-// are Assuming this is always run inside of its docker container, it shouldnt
-// be a problem
-compiler_meta.compiler_dir = "/polytracker/build/bin/polytracker/";
-
-std::vector<std::string> new_argv;
-int new_argc = 0;
-if (strcmp(argv[1], "--instrument") == 0) {
-                                          //Uses opt to load/instrument the pass
-PolyInstrument(argc, argv, new_argc, new_argv);
-}
-else {
-     //Builds target with gllvm
-PolyBuild(argc, argv, new_argc, new_argv);
-const char **final_argv = new const char *[new_argv.size() + 1];
-int i;
-std::string final_command;
-for (i = 0; i < new_argc; i++) {
-#ifdef DEBUG_INFO
-fprintf(stderr, "Arg is: %s\n", new_argv[i].c_str());
-final_command.append(new_argv[i] + " ";
-#endif
-final_argv[i] = new_argv[i].c_str();
-}
-#ifdef DEBUG_INFO
-fprintf(stderr, "I IS: %d\n", i);
-std::cout << final_command << std::endl;
-fprintf(stderr, "====================\n");
-#endif
-final_argv[i] = NULL;
-final_argv[new_argv.size()] = NULL;
-if (execvp(final_argv[0], (char **)final_argv) == -1) {
-fprintf(stderr, "Error: polyclang failed to exec clang: %s\n",
-strerror(errno));
-return -1;
-}
-}
-return 0;
-}
-"""
