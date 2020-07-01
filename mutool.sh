740db10a2d4eb64e11247d7cd5200a5802d843f2
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-07-01 17:56:19 -0400

    Update the mutool script with new paths

--- mutool.sh
+++ mutool.sh
@@ -4,7 +4,7 @@ if [[ "$(docker images -q trailofbits/polytracker 2> /dev/null)" == "" ]]; then
     docker build -t trailofbits/polytracker .
 fi
 if [[ "$(docker images -q trailofbits/polytracker-demo-mupdf 2> /dev/null)" == "" ]]; then
-    docker build -t trailofbits/polytracker-demo-mupdf -f Dockerfile-mupdf.demo .
+    docker build -t trailofbits/polytracker-demo-mupdf -f examples/Dockerfile-mupdf.demo .
 fi
 
-docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo-mupdf:latest mutool draw "$1"
+docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo-mupdf:latest /polytracker/the_klondike/mupdf/build/debug/mutool_track draw "$1"
