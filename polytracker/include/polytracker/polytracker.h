4517ae51d560f978e2c73fde394ae8bd6a55e080
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-07-21 22:29:28 -0400

    Automatically update the Python package version from the C++ header

--- polytracker/include/polytracker/polytracker.h
+++ polytracker/include/polytracker/polytracker.h
@@ -5,8 +5,9 @@
 #define POLYTRACKER_VERSION_MINOR 0
 #define POLYTRACKER_VERSION_REVISION 0
 
-// Set the version note to an empty string if there is no note
-#define POLYTRACKER_SUFFIX "alpha2.2"
+// If there is a suffix, it should always start with a hypen, like "-alpha2.2".
+// If there is no suffix, set POLYTRACKER_VERSION_SUFFIX to an empty string.
+#define POLYTRACKER_VERSION_SUFFIX "-alpha2.2"
 
 /**********************************************************************************/
 
