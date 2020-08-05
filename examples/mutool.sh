5e630d186a50baa0ec42b48dce0163554c2a0273
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-08-05 15:47:14 -0400

    Fix the relative Docker path

--- examples/mutool.sh
+++ examples/mutool.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$(docker images -q trailofbits/polytracker 2> /dev/null)" == "" ]]; then
-    docker build -t trailofbits/polytracker .
+    docker build -t trailofbits/polytracker ..
 fi
 if [[ "$(docker images -q trailofbits/polytracker-demo-mupdf 2> /dev/null)" == "" ]]; then
     docker build -t trailofbits/polytracker-demo-mupdf -f Dockerfile-mupdf.demo .
