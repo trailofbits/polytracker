01791b8652abd4a2e87834b6ca61e65950023cdc
carsonharmon <carson.harmon@trailofbits.com>
2020-09-22 15:47:22 -0400

    Fix main

--- polytracker/polyprocess/__main__.py
+++ polytracker/polyprocess/__main__.py
@@ -114,9 +114,8 @@ def main():
             exit(1)
         if args.outfile:
             with open(args.outfile, "w") as out_file:
-                # datalog_code = grammars.extract_datalog_grammar(traces, input_files)
                 datalog_code = datalog.DatalogParser(input_file, traces[0])
-                out_file.write(datalog_code.str)
+                out_file.write(datalog_code.val)
         else:
             sys.stderr.write(f"Error: No output file selected, use --outfile")
             exit(1)
