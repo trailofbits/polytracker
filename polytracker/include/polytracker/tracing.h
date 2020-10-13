f251f191bf07e4af63a87ea7e09606d5661690c0
carsonharmon <carson.harmon@trailofbits.com>
2020-10-13 16:21:32 -0400

    Update tracing eventCount to be an atomic

--- polytracker/include/polytracker/tracing.h
+++ polytracker/include/polytracker/tracing.h
@@ -16,18 +16,19 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <atomic>
 
 #include "dfsan/dfsan_types.h"
 #include "polytracker/basic_block_types.h"
 
 namespace polytracker {
 
-extern size_t numTraceEvents;
+extern std::atomic<size_t> numTraceEvents;
 
 struct TraceEvent {
   TraceEvent *previous;
   TraceEvent *next;
-  size_t eventIndex;
+  const size_t eventIndex;
   TraceEvent();
   virtual ~TraceEvent() = default;
 };
