c9ed481c26fbcf2c597fc1fc0742be8b56d7d246
carsonharmon <carson.harmon@trailofbits.com>
2020-10-13 16:20:01 -0400

    Remove use of log manager/taint manager in taint model functions

--- polytracker/src/dfsan_sources/taint_sources.cpp
+++ polytracker/src/dfsan_sources/taint_sources.cpp
@@ -1,4 +1,3 @@
-#include "dfsan/dfsan_log_mgmt.h"
 #include <algorithm>
 #include <assert.h>
 #include <fcntl.h>
@@ -13,10 +12,14 @@
 #include <string>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <wchar.h>
 #include <thread>
 #include <time.h>
 #include <unistd.h>
 #include <vector>
+#include <stdio.h>
+#include "include/dfsan/dfsan.h"
+#include "include/polytracker/taint.h"
 
 #define BYTE 1
 #define EXT_C_FUNC extern "C" __attribute__((visibility("default")))
@@ -28,8 +31,6 @@
 #endif
 
 typedef PPCAT(PPCAT(uint, DFSAN_LABEL_BITS), _t) uint_dfsan_label_t;
-
-extern taintManager *taint_manager;
 
 // To create some label functions
 // Following the libc custom functions from custom.cc
@@ -43,11 +44,11 @@ EXT_C_FUNC int __dfsw_open(const char *path, int oflags, dfsan_label path_label,
 #ifdef DEBUG_INFO
   fprintf(stderr, "open: filename is : %s, fd is %d \n", path, fd);
 #endif
-  if (fd >= 0 && taint_manager->isTracking(path)) {
+  if (fd >= 0 && isTrackingSource(path)) {
 #ifdef DEBUG_INFO
     std::cout << "open: adding new taint info!" << std::endl;
 #endif
-    taint_manager->createNewTaintInfo(path, fd);
+    addDerivedSource(path, fd);
   }
   *ret_label = 0;
   return fd;
@@ -64,11 +65,11 @@ EXT_C_FUNC int __dfsw_openat(int dirfd, const char *path, int oflags,
 #ifdef DEBUG_INFO
   fprintf(stderr, "openat: filename is : %s, fd is %d \n", path, fd);
 #endif
-  if (fd >= 0 && taint_manager->isTracking(path)) {
+  if (fd >= 0 && isTrackingSource(path)) {
 #ifdef DEBUG_INFO
     std::cout << "openat: adding new taint info!" << std::endl;
 #endif
-    taint_manager->createNewTaintInfo(path, fd);
+    addDerivedSource(path, fd);
   }
   *ret_label = 0;
   return fd;
@@ -82,11 +83,11 @@ EXT_C_FUNC FILE *__dfsw_fopen64(const char *filename, const char *mode,
   fprintf(stderr, "### fopen64, filename is : %s, fd is %p \n", filename, fd);
   fflush(stderr);
 #endif
-  if (fd != NULL && taint_manager->isTracking(filename)) {
+  if (fd != NULL && isTrackingSource(filename)) {
 #ifdef DEBUG_INFO
     std::cout << "fopen64: adding new taint info!" << std::endl;
 #endif
-    taint_manager->createNewTaintInfo(filename, fd);
+    addDerivedSource(filename, fd);
   }
   *ret_label = 0;
   return fd;
@@ -99,11 +100,11 @@ EXT_C_FUNC FILE *__dfsw_fopen(const char *filename, const char *mode,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### fopen, filename is : %s, fd is %p \n", filename, fd);
 #endif
-  if (fd != NULL && taint_manager->isTracking(filename)) {
+  if (fd != NULL && isTrackingSource(filename)) {
 #ifdef DEBUG_INFO
     std::cout << "fopen: adding new taint info!" << std::endl;
 #endif
-    taint_manager->createNewTaintInfo(filename, fd);
+    addDerivedSource(filename, fd);
   }
 
   *ret_label = 0;
@@ -116,8 +117,8 @@ EXT_C_FUNC int __dfsw_close(int fd, dfsan_label fd_label,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### close, fd is %d , ret is %d \n", fd, ret);
 #endif
-  if (ret == 0 && taint_manager->isTracking(fd)) {
-    taint_manager->closeSource(fd);
+  if (ret == 0 && isTrackingSource(fd)) {
+    closeSource(fd);
   }
   *ret_label = 0;
   return ret;
@@ -129,8 +130,8 @@ EXT_C_FUNC int __dfsw_fclose(FILE *fd, dfsan_label fd_label,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### close, fd is %p, ret is %d \n", fd, ret);
 #endif
-  if (ret == 0 && taint_manager->isTracking(fd)) {
-    taint_manager->closeSource(fd);
+  if (ret == 0 && isTrackingSource(fd)) {
+    closeSource(fd);
   }
   *ret_label = 0;
   return ret;
@@ -147,9 +148,9 @@ EXT_C_FUNC ssize_t __dfsw_read(int fd, void *buff, size_t size,
           size);
 #endif
   // Check if we are tracking this fd.
-  if (taint_manager->isTracking(fd)) {
+  if (isTrackingSource(fd)) {
     if (ret_val > 0) {
-      taint_manager->taintData(fd, (char *)buff, read_start, ret_val);
+      taintData(fd, (char *)buff, read_start, ret_val);
     }
     *ret_label = 0;
   } else {
@@ -164,9 +165,9 @@ EXT_C_FUNC ssize_t __dfsw_pread(int fd, void *buf, size_t count, off_t offset,
                                 dfsan_label offset_label,
                                 dfsan_label *ret_label) {
   ssize_t ret = pread(fd, buf, count, offset);
-  if (taint_manager->isTracking(fd)) {
+  if (isTrackingSource(fd)) {
     if (ret > 0) {
-      taint_manager->taintData(fd, (char *)buf, offset, ret);
+      taintData(fd, (char *)buf, offset, ret);
     }
     *ret_label = 0;
   } else {
@@ -184,9 +185,9 @@ EXT_C_FUNC ssize_t __dfsw_pread64(int fd, void *buf, size_t count, off_t offset,
   std::cout << "Inside of pread64" << std::endl;
 #endif
   ssize_t ret = pread(fd, buf, count, offset);
-  if (taint_manager->isTracking(fd)) {
+  if (isTrackingSource(fd)) {
     if (ret > 0) {
-      taint_manager->taintData(fd, (char *)buf, offset, ret);
+      taintData(fd, (char *)buf, offset, ret);
     }
     *ret_label = 0;
   } else {
@@ -205,10 +206,10 @@ EXT_C_FUNC size_t __dfsw_fread(void *buff, size_t size, size_t count, FILE *fd,
   fprintf(stderr, "### fread, fd is %p \n", fd);
   fflush(stderr);
 #endif
-  if (taint_manager->isTracking(fd)) {
+  if (isTrackingSource(fd)) {
     if (ret > 0) {
       // fread returns number of objects read specified by size
-      taint_manager->taintData(fd, (char *)buff, offset, ret * size);
+      taintData(fd, (char *)buff, offset, ret * size);
     }
     *ret_label = 0;
   } else {
@@ -233,9 +234,9 @@ EXT_C_FUNC size_t __dfsw_fread_unlocked(void *buff, size_t size, size_t count,
   fprintf(stderr, "### fread_unlocked %p,range is %ld, %ld/%ld\n", fd, offset,
           ret, count);
 #endif
-  if (taint_manager->isTracking(fd)) {
+  if (isTrackingSource(fd)) {
     if (ret > 0) {
-      taint_manager->taintData(fd, (char *)buff, offset, ret * size);
+      taintData(fd, (char *)buff, offset, ret * size);
     }
     *ret_label = 0;
   } else {
@@ -251,9 +252,9 @@ EXT_C_FUNC int __dfsw_fgetc(FILE *fd, dfsan_label fd_label,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### fgetc %p, range is %ld, 1 \n", fd, offset);
 #endif
-  if (c != EOF && taint_manager->isTracking(fd)) {
-    *ret_label = taint_manager->createReturnLabel(
-        offset, taint_manager->getTargetInfo(fd)->target_name);
+  if (c != EOF && isTrackingSource(fd)) {
+    *ret_label = createReturnLabel(
+        offset, getSourceName(fd));
   }
   return c;
 }
@@ -266,9 +267,9 @@ EXT_C_FUNC int __dfsw_fgetc_unlocked(FILE *fd, dfsan_label fd_label,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### fgetc_unlocked %p, range is %ld, 1 \n", fd, offset);
 #endif
-  if (c != EOF && taint_manager->isTracking(fd)) {
-    *ret_label = taint_manager->createReturnLabel(
-        offset, taint_manager->getTargetInfo(fd)->target_name);
+  if (c != EOF && isTrackingSource(fd)) {
+    *ret_label = createReturnLabel(
+        offset, getSourceName(fd));
   }
   return c;
 }
@@ -281,9 +282,9 @@ EXT_C_FUNC int __dfsw__IO_getc(FILE *fd, dfsan_label fd_label,
   fprintf(stderr, "### _IO_getc %p, range is %ld, 1 , c is %d\n", fd, offset,
           c);
 #endif
-  if (taint_manager->isTracking(fd) && c != EOF) {
-    *ret_label = taint_manager->createReturnLabel(
-        offset, taint_manager->getTargetInfo(fd)->target_name);
+  if (isTrackingSource(fd) && c != EOF) {
+    *ret_label = createReturnLabel(
+        offset, getSourceName(fd));
   }
   return c;
 }
@@ -296,8 +297,8 @@ EXT_C_FUNC int __dfsw_getchar(dfsan_label *ret_label) {
   fprintf(stderr, "### getchar stdin, range is %ld, 1 \n", offset);
 #endif
   if (c != EOF) {
-    *ret_label = taint_manager->createReturnLabel(
-        offset, taint_manager->getTargetInfo(stdin)->target_name);
+    *ret_label = createReturnLabel(
+        offset, getSourceName(stdin));
   }
   return c;
 }
@@ -310,9 +311,9 @@ EXT_C_FUNC char *__dfsw_fgets(char *str, int count, FILE *fd,
 #ifdef DEBUG_INFO
   fprintf(stderr, "fgets %p, range is %ld, %ld \n", fd, offset, strlen(ret));
 #endif
-  if (ret && taint_manager->isTracking(fd)) {
+  if (ret && isTrackingSource(fd)) {
     int len = strlen(ret);
-    taint_manager->taintData(fd, str, offset, len);
+    taintData(fd, str, offset, len);
     *ret_label = str_label;
   } else {
     *ret_label = 0;
@@ -327,7 +328,7 @@ EXT_C_FUNC char *__dfsw_gets(char *str, dfsan_label str_label,
   fprintf(stderr, "gets stdin, range is %ld, %ld \n", offset, strlen(ret) + 1);
 #endif
   if (ret) {
-    taint_manager->taintData(stdin, str, offset, strlen(ret));
+    taintData(stdin, str, offset, strlen(ret));
     *ret_label = str_label;
   } else {
     *ret_label = 0;
@@ -346,8 +347,8 @@ EXT_C_FUNC ssize_t __dfsw_getdelim(char **lineptr, size_t *n, int delim,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### getdelim %p,range is %ld, %ld\n", fd, offset, ret);
 #endif
-  if (ret > 0 && taint_manager->isTracking(fd)) {
-    taint_manager->taintData(fd, *lineptr, offset, ret);
+  if (ret > 0 && isTrackingSource(fd)) {
+    taintData(fd, *lineptr, offset, ret);
   }
   *ret_label = 0;
   return ret;
@@ -364,8 +365,8 @@ EXT_C_FUNC ssize_t __dfsw___getdelim(char **lineptr, size_t *n, int delim,
 #ifdef DEBUG_INFO
   fprintf(stderr, "### __getdelim %p,range is %ld, %ld\n", fd, offset, ret);
 #endif
-  if (ret > 0 && taint_manager->isTracking(fd)) {
-    taint_manager->taintData(fd, *lineptr, offset, ret);
+  if (ret > 0 && isTrackingSource(fd)) {
+    taintData(fd, *lineptr, offset, ret);
   }
   *ret_label = 0;
   return ret;
@@ -377,8 +378,8 @@ EXT_C_FUNC void *__dfsw_mmap(void *start, size_t length, int prot, int flags,
                              dfsan_label flags_label, dfsan_label fd_label,
                              dfsan_label offset_label, dfsan_label *ret_label) {
   void *ret = mmap(start, length, prot, flags, fd, offset);
-  if (ret && taint_manager->isTracking(fd)) {
-    taint_manager->taintData(fd, (char *)ret, offset, length);
+  if (ret && isTrackingSource(fd)) {
+    taintData(fd, (char *)ret, offset, length);
   }
   *ret_label = 0;
   return ret;
