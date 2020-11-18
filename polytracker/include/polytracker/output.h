fda231cc5d40207586f4126c598c6a06429b5562
carsonharmon <carson.harmon@trailofbits.com>
2020-11-18 15:36:07 -0500

    Update output func/bump polytracker version

--- polytracker/include/polytracker/output.h
+++ polytracker/include/polytracker/output.h
@@ -1,5 +1,5 @@
 #ifndef POLYTRACKER_OUTPUT
 #define POLYTRACKER_OUTPUT
 #include "polytracker/logging.h"
-void output(const char * outfile, const RuntimeInfo* runtime_info);
+void output(const char * forest_path, const char * db_path, const RuntimeInfo* runtime_info, const size_t& curren_thread);
 #endif
