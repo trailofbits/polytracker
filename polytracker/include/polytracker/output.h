94b481de985786b2566936212d2393444b9e668f
carsonharmon <carson.harmon@trailofbits.com>
2020-12-08 15:36:43 -0500

    Add new function overload for output settings & fix typos

--- polytracker/include/polytracker/output.h
+++ polytracker/include/polytracker/output.h
@@ -1,5 +1,6 @@
 #ifndef POLYTRACKER_OUTPUT
 #define POLYTRACKER_OUTPUT
 #include "polytracker/logging.h"
-void output(const char * forest_path, const char * db_path, const RuntimeInfo* runtime_info, const size_t& curren_thread);
+void output(const std::string& forest_path, const std::string& db_path, const RuntimeInfo* runtime_info, const size_t& current_thread);
+void output(const std::string& db_path, const RuntimeInfo* runtime_info, const size_t& current_thread);
 #endif
