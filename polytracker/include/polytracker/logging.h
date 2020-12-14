35d06184f7cc7a1454f904fad0530c6fcb2de139
carsonharmon <carson.harmon@trailofbits.com>
2020-12-14 16:49:07 -0500

    Remove old comments and old defines

--- polytracker/include/polytracker/logging.h
+++ polytracker/include/polytracker/logging.h
@@ -3,11 +3,6 @@
 #include "dfsan/dfsan_types.h"
 #include "polytracker/tracing.h"
 
-//TODO change to enum
-#define UNKNOWN_EVENT_ID -1
-#define INPUT_ACCESS_TYPE 0
-#define CMP_ACCESS_TYPE 1
-
 using namespace polytracker;
 
 [[nodiscard]] taint_node_t *getTaintNode(dfsan_label label);
@@ -22,18 +17,8 @@ void logBBEntry(const char* fname, BBIndex bbIndex, BasicBlockType bbType);
 
 /*
 Each thread has a threadlocal variable which represents its runtime state.
-tFuncStack is the current call stack which records calls/returns to create the
-runtime cfg tainted_funcs_all_ops is a map from function_name --> set<labels>
-that the function operated on tainted_funcs_cmp is the same thing but we wanted
-to make comparisons special and denote it, this might be removed later once we
-do basic block summarization the runtime cfg is the flow sensitive runtime
-control flow graph
 */
 struct RuntimeInfo {
-  //std::vector<func_index_t> tFuncStack;
-  //std::unordered_map<func_index_t, std::unordered_set<dfsan_label>> tainted_funcs_all_ops;
-  //std::unordered_map<func_index_t, std::unordered_set<dfsan_label>> tainted_funcs_cmp;
-  //std::unordered_map<func_index_t, std::unordered_set<func_index_t>> runtime_cfg;
   std::unordered_map<std::string, BBIndex> func_name_to_index;
   polytracker::Trace trace;
 };
