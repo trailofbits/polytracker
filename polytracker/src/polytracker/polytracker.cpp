2ca3019948def31894136306bc18fb40c81d2228
carsonharmon <carson.harmon@trailofbits.com>
2021-02-01 14:42:15 -0500

    Modify stubs for testing with decoupling

--- polytracker/src/polytracker/polytracker.cpp
+++ polytracker/src/polytracker/polytracker.cpp
@@ -1,12 +1,13 @@
 #include "polytracker/taint.h"
 #include "polytracker/logging.h"
-
+#include <iostream>
 
 extern "C" void __polytracker_log_taint_op(dfsan_label label) {
+    std::cout << "LOG TAINT OP: " << label << std::endl;
 
 }
 extern "C" void __polytracker_log_taint_cmp(dfsan_label cmp) {
-
+    std::cout << "LOG TAINT CMP OP " << cmp << std::endl;
 }
 //TODO This is wrong, we should merge with the sqlite stuff and pull how they do 
 extern "C" void __polytracker_log_func_entry(char * fname, dfsan_label something) {
