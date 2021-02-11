c8a671083f3bd5fa3f21235f1a67e92fa0cb9fcd
carsonharmon <carson.harmon@trailofbits.com>
2021-02-11 10:04:15 -0500

    Add test for non baseline CXX decoupling

--- polytracker/scripts/test_decouple.sh
+++ polytracker/scripts/test_decouple.sh
@@ -1,21 +1,22 @@
-
-cp dfsan_baseline.cpp testme.cpp
-# Grab the bitcode
-# This was -o before
-gclang++ -stdlib=libc++ -fPIC -fPIE -Icxx_libs/clean_build/include/c++/v1 -g -c testme.cpp 
+rm testme.*
+rm track.*
+cp test_object_propagation.cpp testme.cpp
+# Static, make sure that you pull in all of libcxx 
+gclang++ -stdlib=libc++ -static -fPIC -fPIE -I/cxx_libs/clean_build/include/c++/v1 -L/cxx_libs/clean_build/lib -g -o testme.o testme.cpp -Wl,--start-group /cxx_libs/clean_build/lib/libc++.a /cxx_libs/clean_build/lib/libc++abi.a -ldl -lpthread -Wl,--end-group
 #-Wl,--start-group -lpthread -lc++abi -lc++ -Wl,--end-group
 
 #mv test_object_propagation.o testme
 get-bc -b testme.o
 
-# Run it through opt, are there any errors?
-opt-10 -load build/share/polytracker/pass/libPolytrackerPass.so -ptrack testme.o.bc -o output.bc
+# Bitcode passes
+opt -load build/share/polytracker/pass/libPolytrackerPass.so -ptrack testme.o.bc -o output.bc
+opt -dfsan -dfsan-abilist=polytracker_abilist.txt output.bc -o track.bc
+opt -O3 track.bc -o track.bc
 
-# Run it through with dfsan
-opt-10 -dfsan -dfsan-abilist=polytracker_abilist.txt output.bc -o track.bc
-clang++ -static -fPIC -fPIE -c track.bc
+# Lower into an object to prevent more transform
+gclang++ -fPIC -c track.bc
 
-echo "LOWERING"
-clang++ -stdlib=libc++ -fsanitize=dataflow -pie -fPIC -fPIE -Icxx_libs/clean_build/include/c++/v1 -Lcxx_libs/clean_build/lib -g -o trackme track.o -Wl,--allow-multiple-definition -Wl,--start-group -lc++abi cxx_libs/poly_build/lib/libc++.a build/share/polytracker/lib/libPolytracker.a -lpthread -Wl,--end-group
+# Link time
+gclang++ -pie -L/cxx_libs/clean_build/lib -g -o trackme track.o -Wl,--allow-multiple-definition -Wl,--start-group -lc++abi /cxx_libs/poly_build/lib/libc++.a build/share/polytracker/lib/libPolytracker.a -lpthread -ldl libclang_rt.dfsan-x86_64.a -Wl,--end-group
 
 #-Wl,--allow-multiple-definition
