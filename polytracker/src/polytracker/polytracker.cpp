91dafeda5a9727af3155d9ba59cf64da3c3f8312
carsonharmon <carson.harmon@trailofbits.com>
2021-02-11 20:21:59 -0500

    Fill in stub functionality

--- polytracker/src/polytracker/polytracker.cpp
+++ polytracker/src/polytracker/polytracker.cpp
@@ -1,43 +1,50 @@
 #include "polytracker/taint.h"
 #include "polytracker/logging.h"
+#include "polytracker/tracing.h"
+#include "polytracker/polytracker.h"
 #include <iostream>
+
+extern bool polytracker_trace_func;
+extern bool polytracker_trace;
 
 extern "C" void __polytracker_log_taint_op(dfsan_label label) {
     if (label != 0) {
-        std::cout << "LOG TAINT OP: " << label << std::endl;
+        logOperation(label);
     }
-
 }
 extern "C" void __polytracker_log_taint_cmp(dfsan_label cmp) {
     if (cmp != 0) {
-        std::cout << "LOG TAINT CMP OP " << cmp << std::endl;
+        logCompare(cmp);
     }
 }
-//TODO This is wrong, we should merge with the sqlite stuff and pull how they do 
-extern "C" void __polytracker_log_func_entry(char * fname, dfsan_label something) {
 
+extern "C" void __polytracker_log_func_entry(char * fname, uint32_t index) {
+    logFunctionEntry(fname, BBIndex(index, 0));
 }
 
-extern "C" void __polytracker_log_func_exit(dfsan_label idk) {
-
+extern "C" void __polytracker_log_func_exit(uint32_t func_index) {
+  logFunctionExit(BBIndex(func_index));
 }
 
-extern "C" void __polytracker_log_bb_entry(char* name, uint32_t findex, uint32_t bindex, uint8_t*idk) {
-
+extern "C" void __polytracker_log_bb_entry(char* name, uint32_t findex, uint32_t bindex, uint8_t btype) {
+  if (polytracker_trace) {
+    logBBEntry(name, BBIndex(findex, bindex),
+               static_cast<polytracker::BasicBlockType>(btype));
+  }
 }
 
-extern "C" void __dfsan_update_label_count(dfsan_label new_label);
+//extern "C" void __dfsan_update_label_count(dfsan_label new_label);
 
 extern "C" dfsan_label __polytracker_union(dfsan_label l1, dfsan_label l2, dfsan_label curr_max) {
-    //TODO Do internal stuff. 
-    //TODO Lock?
-    __dfsan_update_label_count(curr_max + 1);
-    return curr_max + 1;
+    dfsan_label ret = createUnionLabel(l1, l2);
+    //__dfsan_update_label_count(ret);
+    return ret;
 }
 
-extern "C" void __polytracker_dump(dfsan_label last_label) {
-  
+extern "C" void __polytracker_dump(const dfsan_label last_label) {
+    polytracker_end(last_label);
 }
+
 extern "C" int __polytracker_has_label(dfsan_label label, dfsan_label elem) {
     return false;
 }
