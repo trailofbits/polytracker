49765a45df1b46a5b827c4f70b62656b07b1a25a
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-04-02 13:17:52 -0400

    Check for uninstrumented edge cases

--- polytracker/src/polytracker/logging.cpp
+++ polytracker/src/polytracker/logging.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 #include <mutex>
 #include <set>
-#include <stack>
 #include <thread>
 #include <tuple>
 #include <unordered_map>
@@ -18,7 +17,7 @@ extern input_id_t input_id;
 extern sqlite3 *output_db;
 thread_local block_id_t curr_block_index = -1;
 thread_local function_id_t curr_func_index = -1;
-thread_local std::stack<event_id_t> function_stack;
+thread_local FunctionStack function_stack;
 thread_local int thread_id = -1;
 thread_local event_id_t thread_event_id = 0;
 std::atomic<event_id_t> event_id = 0;
@@ -46,7 +45,7 @@ void logCompare(const dfsan_label &label, const function_id_t &findex,
   storeTaintAccess(output_db, label, this_event_id, thread_event_id++, findex,
                    bindex, input_id, thread_id, ByteAccessType::CMP_ACCESS,
                    function_stack.empty() ? this_event_id
-                                          : function_stack.top());
+                                          : function_stack.top().func_event_id);
 }
 
 void logOperation(const dfsan_label &label, const function_id_t &findex,
@@ -55,7 +54,7 @@ void logOperation(const dfsan_label &label, const function_id_t &findex,
   storeTaintAccess(output_db, label, event_id++, thread_event_id++, findex,
                    bindex, input_id, thread_id, ByteAccessType::INPUT_ACCESS,
                    function_stack.empty() ? this_event_id
-                                          : function_stack.top());
+                                          : function_stack.top().func_event_id);
 }
 
 thread_local bool recursive = false;
@@ -72,6 +71,7 @@ void logFunctionEntry(const char *fname, const function_id_t &func_id) {
     is_init = true;
     polytracker_start();
   }
+  // std::cerr << "logFunctionEntry(" << fname << ", " << func_id << ")\n";
   // TODO (Carson) for Evan, its just a check quick to assign a thread_id.
   assignThreadID();
   // This just stores a mapping, should be true for all runs.
@@ -82,7 +82,7 @@ void logFunctionEntry(const char *fname, const function_id_t &func_id) {
                    this_event_id, EdgeType::FORWARD);
   storeEvent(output_db, input_id, thread_id, this_event_id, thread_event_id++,
              EventType::FUNC_ENTER, func_id, 0, this_event_id);
-  function_stack.push(this_event_id);
+  function_stack.push({this_event_id, func_id});
   if (UNLIKELY(func_id == curr_func_index)) {
     recursive_funcs[func_id] = true;
   }
@@ -92,29 +92,32 @@ void logFunctionEntry(const char *fname, const function_id_t &func_id) {
 void logFunctionExit(const function_id_t &index) {
   if (UNLIKELY(!is_init)) {
     return;
+  } else if (UNLIKELY(function_stack.empty() ||
+                      function_stack.top().func_id != curr_func_index)) {
+    std::cerr
+        << "Warning: Could not resolve the function entry associated with "
+           "the return from function index "
+        << curr_func_index << " to " << index;
+    if (!function_stack.empty()) {
+      std::cerr << "(expected to be returning from function "
+                << function_stack.top().func_id << ")";
+    }
+    std::cerr << ". This is likely due to either an instrumentation error "
+              << "or non-standard control-flow in the instrumented program.\n";
   }
   // Here, the curr_func_index is from the function we just returned from
   // NOTE (Carson) the map makes sure we don't add accidental recursive edges
   // due to missing instrumentation Draw return edge from curr_func_index -->
   // index
-  if (curr_func_index != index ||
-      (recursive_funcs.find(curr_func_index) != recursive_funcs.end())) {
+  else if (curr_func_index != index ||
+           (recursive_funcs.find(curr_func_index) != recursive_funcs.end())) {
+    // std::cerr << "logFunctionExit(" << index << ")\n";
     const auto this_event_id = event_id++;
     storeFuncCFGEdge(output_db, input_id, thread_id, index, curr_func_index,
                      this_event_id, EdgeType::BACKWARD);
     event_id_t current_function_event;
-    if (UNLIKELY(function_stack.empty())) {
-      std::cerr
-          << "Warning: Could not resolve the function entry associated with "
-             "the return from function index "
-          << curr_func_index << " to " << index
-          << ". This is likely due to either an instrumentation error "
-          << "or non-standard control-flow in the instrumented program.\n";
-      current_function_event = this_event_id;
-    } else {
-      current_function_event = function_stack.top();
-      function_stack.pop();
-    }
+    current_function_event = function_stack.top().func_event_id;
+    function_stack.pop();
     storeEvent(output_db, input_id, thread_id, this_event_id, thread_event_id++,
                EventType::FUNC_RET, index, 0, current_function_event);
   }
@@ -130,6 +133,7 @@ void logBBEntry(const char *fname, const function_id_t &findex,
   const auto this_event_id = event_id++;
   storeEvent(output_db, input_id, thread_id, this_event_id, thread_event_id++,
              EventType::BLOCK_ENTER, findex, bindex,
-             function_stack.empty() ? this_event_id : function_stack.top());
+             function_stack.empty() ? this_event_id
+                                    : function_stack.top().func_event_id);
   curr_block_index = bindex;
 }
