61dd7e4b88867284662cdaadb4604a87994b3c3c
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-04-06 16:34:22 -0400

    Remove the taint forest commands for now

--- polytracker/polytracker.py
+++ polytracker/polytracker.py
@@ -12,31 +12,31 @@ def version() -> str:
     return pkg_resources.require("polytracker")[0].version
 
 
-class TaintForestCommand(Command):
-    name = "forest"
-    help = "commands related to the taint forest"
-    parser: ArgumentParser
-
-    def __init_arguments__(self, parser: ArgumentParser):
-        self.parser = parser
-
-    def run(self, args: Namespace):
-        self.parser.print_help()
-
-
-class DrawTaintForestCommand(Subcommand[TaintForestCommand]):
-    name = "draw"
-    help = "render the taint forest to a Graphviz .dot file"
-    parent_type = TaintForestCommand
-
-    def __init_arguments__(self, parser):
-        parser.add_argument(
-            "taint_forest_bin", type=str, help="the taint forest file for the trace"
-        )
-        parser.add_argument(
-            "output_dot_path", type=str, help="the path to which to save the .dot graph"
-        )
-
-    def run(self, args: Namespace):
-        forest = TaintForest(args.taint_forest_bin)
-        forest.to_graph().to_dot().save(args.output_dot_path)
+# class TaintForestCommand(Command):
+#     name = "forest"
+#     help = "commands related to the taint forest"
+#     parser: ArgumentParser
+#
+#     def __init_arguments__(self, parser: ArgumentParser):
+#         self.parser = parser
+#
+#     def run(self, args: Namespace):
+#         self.parser.print_help()
+#
+#
+# class DrawTaintForestCommand(Subcommand[TaintForestCommand]):
+#     name = "draw"
+#     help = "render the taint forest to a Graphviz .dot file"
+#     parent_type = TaintForestCommand
+#
+#     def __init_arguments__(self, parser):
+#         parser.add_argument(
+#             "taint_forest_bin", type=str, help="the taint forest file for the trace"
+#         )
+#         parser.add_argument(
+#             "output_dot_path", type=str, help="the path to which to save the .dot graph"
+#         )
+#
+#     def run(self, args: Namespace):
+#         forest = TaintForest(args.taint_forest_bin)
+#         forest.to_graph().to_dot().save(args.output_dot_path)
