2ce7d8f47439d20a87bc0e4df031861df1ac9f5f
carsonharmon <carson.harmon@trailofbits.com>
2021-06-29 16:28:48 -0400

    Support scripts for more symbol generation

---
+++ polytracker/scripts/cxx_syms.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+DUMP_PASS_LIB: str = sys.argv[1]
+CXX_BC_DIR: str = sys.argv[2]
+OUTFILE: str = sys.argv[3]
+assert os.path.exists(CXX_BC_DIR)
+
+items = os.listdir(CXX_BC_DIR)
+for item in items:
+    os.system(f"opt --enable-new-pm=0 -load {DUMP_PASS_LIB} --dump -o doesnt_matter.bc {item} >> {OUTFILE}")
