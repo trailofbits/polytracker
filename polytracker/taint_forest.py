2e8abe1eb11a91878f6355d7756cb192a1da2911
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-11-03 11:39:53 -0400

    Stubbed out a command for exporting the taint forest to DOT

--- polytracker/taint_forest.py
+++ polytracker/taint_forest.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod
 from typing import Iterator, Optional
 
+from .graphs import DAG
 from .inputs import Input
+from .plugins import Command
 
 
 class TaintForestNode:
@@ -34,9 +36,31 @@ class TaintForest:
     def nodes(self) -> Iterator[TaintForestNode]:
         raise NotImplementedError()
 
+    def to_graph(self) -> DAG[TaintForestNode]:
+        # TODO
+        raise NotImplementedError()
+
     def __iter__(self):
         return self.nodes()
 
     @abstractmethod
     def __len__(self):
         raise NotImplementedError()
+
+
+class ExportTaintForest(Command):
+    name = "forest"
+    help = "export a taint forest to GraphViz (DOT) format"
+
+    def __init_arguments__(self, parser):
+        parser.add_argument("POLYTRACKER_DB", type=str, help="the trace database")
+        parser.add_argument("OUTPUT_PATH", type=str, help="path to which to save the .dot file")
+
+    def run(self, args):
+        from . import PolyTrackerTrace
+
+        trace = PolyTrackerTrace.load(args.POLYTRACKER_DB)
+        graph = trace.taint_forest.to_graph()
+        graph.to_dot().save(args.OUTPUT_PATH)
+        print(f"Exported the taint forest to {args.OUTPUT_PATH}")
+        print(f"To render it to a PDF, run `dot -Tpdf -o taint_forest.pdf {args.OUTPUT_PATH}`")
