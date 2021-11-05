982b7145ec0015a9e6cc950d9455e0f06e327895
hbrodin <90325907+hbrodin@users.noreply.github.com>
2021-11-05 14:24:05 +0100

    Rewrite of gfanalyze, fix some issues

--- src/gfanalyze/gfanalyze.cpp
+++ src/gfanalyze/gfanalyze.cpp
@@ -1,265 +1,228 @@
 
 #include <llvm/Bitcode/BitcodeWriter.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Module.h>
 #include <llvm/IR/Constants.h>
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
 #include <llvm/IRReader/IRReader.h>
-#include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/FileSystem.h>
+#include <llvm/Support/SourceMgr.h>
 #include <llvm/Transforms/Utils/Cloning.h>
 
-#include<algorithm>
-#include <stack>
+#include <algorithm>
+#include <deque>
 #include <unordered_map>
 
-#include "gigafunction/types.h"
 #include "gigafunction/pass/mark_basic_blocks.h"
 #include "gigafunction/traceio/trace_reader.h"
+#include "gigafunction/types.h"
 
 using namespace llvm;
 
 namespace gigafunction {
-using blockindex_t = std::unordered_map<block_id, BasicBlock*>;
-
-
-
+using blockindex_t = std::unordered_map<block_id, BasicBlock *>;
 
 // TODO (hbrodin): This was stolen from instrument_basic_blocks.cpp create lib.
-ConstantInt* read_block_id(BasicBlock &bb) {
-  if (MDNode* n = bb.front().getMetadata(gigafunction::get_metadata_tag())) {
+ConstantInt *read_block_id(BasicBlock &bb) {
+  if (MDNode *n = bb.front().getMetadata(gigafunction::get_metadata_tag())) {
     auto n0 = cast<ConstantAsMetadata>(n->getOperand(0))->getValue();
     return cast<ConstantInt>(n0);
   }
-  errs() << "Warning did not find metadata tag '" << gigafunction::get_metadata_tag() << 
-        "' for block in function " << bb.getParent()->getName() << "\n" <<
-        " ensure the markbasicblocks-pass have been run previously.";
+  errs() << "Warning did not find metadata tag '"
+         << gigafunction::get_metadata_tag() << "' for block in function "
+         << bb.getParent()->getName() << "\n"
+         << " ensure the markbasicblocks-pass have been run previously.";
   return nullptr;
 }
-struct call_state {
-  BasicBlock *alloca_bb;
-  BasicBlock *bind_ret_bb;
+
+// Create a branch from->to. If last instr in from is a branch of any kind
+// update it to to
+void ensure_branch(BasicBlock *from, BasicBlock *to) {
+  if (auto branch = dyn_cast<BranchInst>(&from->getInstList().back())) {
+    if (branch->isUnconditional()) {
+      branch->setOperand(0, to);
+      return; // Early abort, just change branch target
+    }
+    // drop conditional branch
+    branch->eraseFromParent();
+  }
+
+  // insert branch
+  IRBuilder<> irb(from);
+  irb.CreateBr(to);
+}
+
+struct frame_state {
+  CallInst *caller{};
+  Value *replace_Call{};
+  // Calls in the current frame
+  SmallVector<CallInst *> calls;
+  SmallVector<std::pair<CallInst *, Value *>> replace_calls;
+
+  // Remapping of values in current frame
   ValueToValueMapTy vm;
-  call_state(BasicBlock *allocabb = nullptr, BasicBlock *bindretbb = nullptr)
-   : alloca_bb(allocabb), bind_ret_bb(bindretbb) {}
+
+  frame_state() {}
+  frame_state(CallInst *caller) : caller(caller) {}
 };
 
-using callstack_ty = std::stack<call_state>;
+using call_frames = std::deque<frame_state>;
 
-// Create a branch from->to. If last instr in from is a branch of any kind update it to to
-void ensure_branch(BasicBlock *from, BasicBlock *to) {
-    if (auto branch = dyn_cast<BranchInst>(&from->getInstList().back())) {
-      if (branch->isUnconditional()) {
-        branch->setOperand(0, to);
-        return; // Early abort, just change branch target
-      }
-      // drop conditional branch
-      branch->removeFromParent();
+struct gigafunction_state {
+  call_frames frames;
+  Function *gigafunction{nullptr};
+  BasicBlock *prev_translated_bb{nullptr};
+};
+
+void on_entry_block(BasicBlock *orig_bb, gigafunction_state &state) {
+  if (state.frames.empty()) {
+    auto orig_func = orig_bb->getParent();
+    state.gigafunction = Function::Create(
+        orig_func->getFunctionType(), llvm::Function::ExternalLinkage,
+        "gigafunction", orig_func->getParent());
+
+    // Initialize empty frame
+    auto &frame = state.frames.emplace_back();
+
+    // Map any references to orginal function arguments to our new gigafunction
+    // args
+    auto gf = state.gigafunction;
+    auto f = orig_bb->getParent();
+    for (auto gf_arg = gf->arg_begin(), f_arg = f->arg_begin();
+         gf_arg != gf->arg_end(); gf_arg++, f_arg++) {
+      frame.vm[f_arg] = gf_arg;
+    }
+  } else {
+    assert(state.prev_translated_bb && "BUG: Inconsistent state. Should have "
+                                       "prev_translated_bb when !cf.empty()");
+    auto caller = cast<CallInst>(
+        &*(++(state.prev_translated_bb->rbegin()))); // Bug if not a call here
+
+    // append this call to parent frame calls to be removed
+    state.frames.back().calls.emplace_back(caller);
+
+    // Create a new frame originating from caller
+    auto &frame = state.frames.emplace_back(caller);
+
+    // Map all orig_bb arguments to caller operands
+    auto f = orig_bb->getParent();
+    auto c_arg = caller->arg_begin();
+    for (auto &arg : f->args()) {
+      assert(c_arg->getOperandNo() == arg.getArgNo());
+      frame.vm[&arg] = cast<Value>(c_arg);
+      ++c_arg;
+    }
+  }
+}
+
+void on_ret_block(ReturnInst *ret, gigafunction_state &state) {
+  // Have parent frame, update caller to be replaced by operand 0 of the ret,
+  // if any
+  auto &current_frame = state.frames.back();
+
+  // Replace any usage of calls in the current frame with their "inlined
+  // versions" finally drop the calls
+  for (auto [call, replace] : current_frame.replace_calls) {
+    if (replace)
+      call->replaceAllUsesWith(replace);
+    call->eraseFromParent();
+  }
+
+  if (state.frames.size() > 1) {
+    auto caller = current_frame.caller;
+    assert(caller && "BUG: Should be a caller if there is a parent frame");
+
+    auto &parent_frame = *(++state.frames.rbegin());
+    if (ret->getNumOperands() == 1) {
+      parent_frame.replace_calls.emplace_back(caller, ret->getOperand(0));
+    } else {
+      parent_frame.replace_calls.emplace_back(caller, nullptr);
     }
 
-    // insert branch
-    IRBuilder<> irb(from);
-    irb.CreateBr(to);
-}
-
-BasicBlock * create_func_args_block(LLVMContext &ctx, Function* gf, Function* f, BasicBlock *prev_bb, callstack_ty &cs) {
-  //auto bb = BasicBlock::Create(ctx, "alloca_bb", gf);
-  auto bb = BasicBlock::Create(ctx, Twine(f->getName()).concat("-args"), gf);
-  IRBuilder<> irb(bb);
-
-  for (auto &arg : f->args()) {
-    auto alloca = irb.CreateAlloca(arg.getType());
-    cs.top().vm[&arg] = alloca;
+    // Drop the ret, it will be replaced by a branch later on
+    ret->eraseFromParent();
   }
 
-  // If a return value, create alloca to store it as well
-  auto ret_ty = f->getReturnType();
-  if (!ret_ty->isVoidTy())
-    irb.CreateAlloca(ret_ty);
-
-  if (prev_bb)
-    ensure_branch(prev_bb, bb);
-  return bb;
+  state.frames.pop_back();
 }
 
-// Top level bind-args is different since we don't have the call
-// just propagate the 'gigafunction' arguments into the alloca block
-BasicBlock *create_top_bind_args_bb(LLVMContext &ctx, Function *gf, BasicBlock *alloc_bb) {
-  auto bind_args_bb = BasicBlock::Create(ctx,  Twine(alloc_bb->getName()).concat("-bind-topargs"), gf);
-  IRBuilder<> irb(bind_args_bb);
+BasicBlock *clone_single_bb(BasicBlock *orig_bb, gigafunction_state &state) {
 
-  auto alloca_ins = &alloc_bb->front();
-  for (auto &arg : gf->args()) {
-    irb.CreateStore(&arg, alloca_ins);
-    alloca_ins = alloca_ins->getNextNode();
-  }
-  ensure_branch(alloc_bb, bind_args_bb);
-  return bind_args_bb;
-}
+  auto &current_frame = state.frames.back();
 
-// Returns block for binding arguments and binding return value
-std::pair<BasicBlock*, BasicBlock*> create_bind_args_bb(LLVMContext &ctx, Function *gf, BasicBlock *prev_bb, BasicBlock *alloc_bb) {
-  // Locate the call instruction, it should be the next last one otherwise something is wrong
-  auto split_ins = cast<CallInst>(&*(++(prev_bb->rbegin())));
-  auto bind_result_bb = prev_bb->splitBasicBlock(split_ins, Twine(alloc_bb->getName()).concat("-result"));
-  auto call_ins = cast<CallInst>(bind_result_bb->begin());
-
-  // Prepare the bind args bb
-  auto bind_args_bb = BasicBlock::Create(ctx, Twine(alloc_bb->getName()).concat("-bind"), gf);
-  IRBuilder<> irb(bind_args_bb);
-
-  auto alloca_ins = &alloc_bb->front();
-  for (auto &arg : call_ins->args()) {
-    irb.CreateStore(&*arg, alloca_ins);
-    alloca_ins = alloca_ins->getNextNode();
-  }
-
-  // If the return type is void, we just drop the call from the bind_resul_bb
-  // Else we need to insert a load
-  auto ret_ty = call_ins->getFunctionType()->getReturnType();
-  if (!ret_ty->isVoidTy()) {
-    IRBuilder<> irb(bind_result_bb);
-    auto ld = irb.CreateLoad(ret_ty, alloca_ins);
-    call_ins->replaceAllUsesWith(ld);
-  }
-  // There will be a branch left. Discard it. It will be updated when a return
-  // instruction is encountered.
-  call_ins->getNextNode()->removeFromParent();
-  // Drop the call. If it returned any value, any use was replaced above.
-  call_ins->removeFromParent();
-  ensure_branch(prev_bb, alloc_bb);
-  ensure_branch(alloc_bb, bind_args_bb);
-
-  return {bind_args_bb, bind_result_bb};
-}
-
-BasicBlock *clone_block(LLVMContext &ctx, Function *gf, BasicBlock *prev_bb, BasicBlock *alloc_bb, BasicBlock *to_clone, callstack_ty &cs) {
-
-  auto &state = cs.top();
-
-  auto new_bb = BasicBlock::Create(ctx, to_clone->getParent()->getName(), gf);
-  state.vm[to_clone] = new_bb; // TODO (hbrodin): Do we need this??
-  for (auto &ins : *to_clone) {
+  auto translated_bb =
+      BasicBlock::Create(state.gigafunction->getContext(),
+                         orig_bb->getParent()->getName(), state.gigafunction);
+  current_frame.vm[orig_bb] =
+      translated_bb; // TODO (hbrodin): Do we need this??
+  for (auto &ins : *orig_bb) {
     auto new_ins = ins.clone();
-    state.vm[&ins] = new_ins;
-    RemapInstruction(new_ins, state.vm,  RF_NoModuleLevelChanges);
-    new_bb->getInstList().push_back(new_ins);
+    current_frame.vm[&ins] = new_ins;
+    RemapInstruction(new_ins, current_frame.vm, RF_NoModuleLevelChanges);
+    translated_bb->getInstList().push_back(new_ins);
   }
 
-  if (prev_bb)
-    ensure_branch(prev_bb, new_bb);
-  return new_bb;
+  if (state.prev_translated_bb)
+    ensure_branch(state.prev_translated_bb, translated_bb);
+  return translated_bb;
 }
 
-
-// Stores whatever is normally returned from the function
-// in the alloca block for the function, for later consumption
-// by the call-replacement. If no return value, just skip store.
-BasicBlock *handle_ret(BasicBlock *bb, callstack_ty& cs) {
-  if (auto ret = dyn_cast<ReturnInst>(&bb->back())) {
-    // This is a ret instruction, complete current function by joining the
-    // load ret value and drop the ret instruction
-    auto &state = cs.top();
-    auto br = state.bind_ret_bb;
-    auto alloca_bb = state.alloca_bb;
-    
-    cs.pop();
-    if (br) {
-      if (ret->getNumOperands() == 1) {
-        IRBuilder<> irb(bb);
-        irb.CreateStore(ret->getOperand(0), &*(++alloca_bb->rbegin()));
-      }
-      ret->removeFromParent();
-      ensure_branch(bb, br);
-      return br;
-    }
-  }
-  return bb;
-}
-
-/*
-
-Link a sequence of basic blocks representing exeuction
-
-Each bb contains at most one call. A successor to a block with a call is either
-part of the same function for uninstrumented calls (e.g. llvm.dbg.declare) or the
-entry block of the new function. If it is an entry block the following split is done
-
-Before: [caller-bb][callee-entry-bb]
-After: [caller-bb/pre-call] [*arg-allocaa] [*arg-bind] [callee-entry-bb] ...[callee-ret] [caller-bb/postcall]
-with * being newly created blocks to handle linking of function arguments/return value
-
-arg-alloca: Allocas for all function arguments and return value
-arg-bind: extract function operands and store them in corresponding arg-alloca
-callee-ret: replace the ret instruction with a store of the return value into arg-alloca
-caller-bb/postcall: replace the call with a load from arg-alloca (return value)
-
-*/
-void create_gigafunction_direct(LLVMContext &ctx, Module &mod, char const *fname, blockindex_t const &bi) {
-
-  Function *gf{nullptr};
-
+// Constructs gigafunctions given a blockindex
+// A blockindex is constructed by running a binary instrumented to emit a
+// block-id for each basic block From each block-id, a chain of basic blocks is
+// copied into a 'gigafunction'. Special handling of function arguments is
+// needed. Need to replace users of call instructions with the value from ret of
+// the called function and also make sure that the operands to the call
+// instruction is bound to the basic blocks representing the callee.
+void create_gigafunctions(LLVMContext &ctx, Module &mod, char const *fname,
+                          blockindex_t const &bi) {
   trace_reader tr(fname);
-  callstack_ty callstack;
+  gigafunction_state state;
 
-
-  BasicBlock *prev_bb{nullptr};
-  for (auto e = tr.next();e;e=tr.next()) {
+  for (auto e = tr.next(); e; e = tr.next()) {
     auto [tid, bid] = e.value();
     auto it = bi.find(bid);
 
     if (it == bi.end()) {
-      errs() << "Broken analysis. Failed to find block " << bid << " in the block index. Abort.\n";
+      errs() << "Broken analysis. Failed to find block " << bid
+             << " in the block index. Abort.\n";
       abort();
     }
 
-    auto bb = it->second;
-
-    // This is the initial block for a function. Prepare blocks for argument/return value store
-    if (bb->isEntryBlock()) {
-      if (!gf) {
-         gf = Function::Create(bb->getParent()->getFunctionType(), llvm::Function::ExternalLinkage, "gigafunction", mod);
-      }
-
-
-      // Allocate storage for arguments/return values
-      callstack.emplace(nullptr);
-      auto alloc_bb = create_func_args_block(ctx, gf, bb->getParent(), prev_bb, callstack);
-      callstack.top().alloca_bb = alloc_bb;
-      if (prev_bb) {
-        // Store parameters to alloc_bb
-        auto [bind_args_bb, bind_results_bb] = create_bind_args_bb(ctx, gf, prev_bb, alloc_bb);
-        callstack.top().bind_ret_bb = bind_results_bb;
-
-        prev_bb = bind_args_bb;
-      } else {
-        auto bind_args_bb = create_top_bind_args_bb(ctx, gf, alloc_bb);
-        prev_bb = bind_args_bb;
-      }
+    auto orig_bb = it->second;
+    if (orig_bb->isEntryBlock()) {
+      on_entry_block(orig_bb, state);
     }
-    // Clone block and chain to prev block
-    prev_bb = clone_block(ctx, gf, prev_bb, callstack.top().alloca_bb, bb, callstack);
 
-    // Check if we just hit a ret-block
-    prev_bb = handle_ret(prev_bb, callstack);
+    auto translated_bb = clone_single_bb(orig_bb, state);
+    if (auto ret = dyn_cast<ReturnInst>(&*translated_bb->rbegin())) {
+      on_ret_block(ret, state);
+    }
 
+    if (state.prev_translated_bb) {
+      ensure_branch(state.prev_translated_bb, translated_bb);
+    }
+
+    state.prev_translated_bb = translated_bb;
   }
+
+  verifyFunction(*state.gigafunction, &outs());
 }
 
 blockindex_t build_block_index(Module &m) {
   gigafunction::blockindex_t blockindex;
-  for (auto& f : m) {
-    for (auto& bb : f) {
+  for (auto &f : m) {
+    for (auto &bb : f) {
       blockindex.emplace(gigafunction::read_block_id(bb)->getZExtValue(), &bb);
     }
   }
   return blockindex;
 }
 
-}
+} // namespace gigafunction
 
 int main(int argc, char *argv[]) {
-
 
   SMDiagnostic error;
   LLVMContext ctx;
@@ -272,13 +235,12 @@ int main(int argc, char *argv[]) {
 
   auto blockindex = gigafunction::build_block_index(*mod);
 
+  gigafunction::create_gigafunctions(ctx, *mod, argv[2], blockindex);
 
-  gigafunction::create_gigafunction_direct(ctx, *mod, argv[2], blockindex);
-  
   if (argc == 4) {
     std::error_code ec;
-    raw_fd_ostream os(argv[2], ec, sys::fs::CD_CreateAlways);
-    if (ec) { 
+    raw_fd_ostream os(argv[3], ec, sys::fs::CD_CreateAlways);
+    if (ec) {
       errs() << ec.message() << "\n";
     }
     WriteBitcodeToFile(*mod, os);
@@ -286,5 +248,4 @@ int main(int argc, char *argv[]) {
   }
 
   return 0;
-
 }
