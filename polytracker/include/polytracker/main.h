0fcd3b0aa7b7a3afc2da4dbc0797d60bebfc162e
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-11-08 16:30:31 -0500

    Update polytracker/include/polytracker/main.h
    Co-authored-by: Henrik Brodin <90325907+hbrodin@users.noreply.github.com>

--- polytracker/include/polytracker/main.h
+++ polytracker/include/polytracker/main.h
@@ -6,6 +6,6 @@ struct block_mapping;
 
 void polytracker_start(func_mapping const *globals, uint64_t globals_count,
                        block_mapping const *block_map, uint64_t block_map_count,
-                       bool control_flow_tracing);
+                       bool control_flow_tracking);
 
 #endif
