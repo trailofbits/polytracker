bb4f07918cdf793902e34014ef62e4591fd3abf6
Evan Sultanik <evan.sultanik@trailofbits.com>
2022-01-26 16:53:11 -0500

    Update the cavities script to run on macos and have better logging

--- build_in_docker/run_file_cavity_detection.sh
+++ build_in_docker/run_file_cavity_detection.sh
@@ -15,52 +15,77 @@ else
   READLINK="readlink"
 fi
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+make -C "${SCRIPTPATH}" "bin/mutool_track_no_control_flow"
+
 cavities(){
   INPUT_PDF="$1"
   RESULT_DIR="$2"
   TIMEOUT="$3"
   BASENAME="${INPUT_PDF%.pdf}"
   BASENAME="${BASENAME##*/}"
-  SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-  make -C "${SCRIPTPATH}" "bin/mutool_track_no_control_flow"
+  echo "Detecting file cavities in ${BASENAME}.pdf ..."
 
   cp "${INPUT_PDF}" "${RESULT_DIR}/${BASENAME}.pdf"
   
-  docker run -it --rm \
+  if docker run -it --rm \
     --mount type=bind,source="${SCRIPTPATH}/bin",target=/sources/bin \
     --mount type=bind,source="${RESULT_DIR}",target=/workdir \
+    --workdir /workdir \
     trailofbits/polytrackerbuilder-mupdf /usr/bin/bash -c \
-    "cd /workdir && timeout ${TIMEOUT} /sources/bin/mutool_track_no_control_flow info ${BASENAME}.pdf; if [ \$? -eq 124 ]; then touch ${BASENAME}.timeout; fi"
-
-  # mv -f "${RESULT_DIR}/output.png" "${RESULT_DIR}/${BASENAME}.png"
-  mv -f "${RESULT_DIR}/polytracker.db" "${RESULT_DIR}/${BASENAME}.db"
-  rm -f "${RESULT_DIR}/${BASENAME}.pdf"
-  
-  if [[ -f "${RESULT_DIR}/${BASENAME}.timeout" ]]
+    "timeout ${TIMEOUT} /sources/bin/mutool_track_no_control_flow info ${BASENAME}.pdf" \
+    > "${RESULT_DIR}/${BASENAME}.mutool.log" 2>&1
   then
-    echo "${BASENAME}.pdf,-1,-1" >> "${RESULT_DIR}/cavities.csv"
-    rm -f "${RESULT_DIR}/${BASENAME}.timeout"
+    # mv -f "${RESULT_DIR}/output.png" "${RESULT_DIR}/${BASENAME}.png"
+    mv -f "${RESULT_DIR}/polytracker.db" "${RESULT_DIR}/${BASENAME}.db"
+    rm -f "${RESULT_DIR}/${BASENAME}.pdf"
+  else
+    retcode=$?
+    if [ $retcode -eq 124 ]; then
+      echo "\`mutool draw ${BASENAME}.pdf\` timed out after ${TIMEOUT} seconds"
+      echo "${BASENAME}.pdf,-1,-1" >> "${RESULT_DIR}/cavities.csv"
+    else
+      echo "\`mutool draw ${BASENAME}.pdf\` exited with code ${retcode}"
+      echo "see ${RESULT_DIR}/${BASENAME}.mutool.log"
+      echo "${BASENAME}.pdf,-3,-3" >> "${RESULT_DIR}/cavities.csv"
+    fi
     return
   fi
-  
-  timeout "${TIMEOUT}" python3 cavities.py "${RESULT_DIR}/${BASENAME}.db" >> "${RESULT_DIR}/cavities.csv"
-  
-  if [ $? -eq 124 ]
-  then
-    echo "${BASENAME}.pdf,-2,-2" >> "${RESULT_DIR}/cavities.csv"
+
+  if timeout "${TIMEOUT}" python3 cavities.py "${RESULT_DIR}/${BASENAME}.db" > .result.csv \
+      2> "${RESULT_DIR}/${BASENAME}.cavities.log"; then
+    cat .result.csv >> "${RESULT_DIR}/cavities.csv"
+  else
+    retcode=$?
+    if [ $retcode -eq 124 ]; then
+      echo "python3 cavities.py \"${RESULT_DIR}/${BASENAME}.db\" timed out after ${TIMEOUT} seconds"
+      echo "${BASENAME}.pdf,-2,-2" >> "${RESULT_DIR}/cavities.csv"
+    else
+      echo "python3 cavities.py \"${RESULT_DIR}/${BASENAME}.db\" exited with code ${retcode}"
+      echo "see ${RESULT_DIR}/${BASENAME}.cavities.log"
+      echo "${BASENAME}.pdf,-4,-4" >> "${RESULT_DIR}/cavities.csv"
+    fi
   fi
+
+  rm -f .result.csv
 
   return
 }
 
-if [[ ! -d "results" ]]
+if [[ -f "results/cavities.csv" ]]
+then
+  echo "results/cavities.csv already exists!"
+  echo "Move or delete it and try again."
+  exit 1
+elif [[ ! -d "results" ]]
 then
   mkdir "results"
 fi
 
 for file in "$@"; do
-  cavities "$(${READLINK} -f ${file})" "$(pwd)/results" 5
+  cavities "$(${READLINK} -f ${file})" "$(pwd)/results" 100
 done
 
 # TODO(surovic): Add printing of the below stats
