d7582e28b53f4713a075c41decdef7db71b2d462
Marek Surovič <marek.surovic@gmail.com>
2022-02-11 14:57:39 +0100

    Changes runner workdir
    Changes runner to create working directories in the output directory.
    Instead of  making them in `/tmp` cause ubuntu docker doesn't like it.

--- build_in_docker/file_cavity_detection.py
+++ build_in_docker/file_cavity_detection.py
@@ -5,33 +5,45 @@ import os.path
 import subprocess
 from argparse import ArgumentParser
 from os import mkdir, rename
+from shutil import rmtree
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from time import time
 from typing import Dict, Iterable, Union
+from contextlib import contextmanager
 
 DOCKER_IMAGE = "trailofbits/polytrackerbuilder-mupdf"
 BINDIR = Path(os.path.dirname(os.path.realpath(__file__))) / "bin"
 MUTOOL_PATH = "/sources/bin/mutool_track_no_control_flow"
 TIMEOUT = 100
-SCRIPTDIR =  Path(os.path.dirname(os.path.realpath(__file__)))
+SCRIPTDIR = Path(os.path.dirname(os.path.realpath(__file__)))
 
 POLYDB = "polytracker.db"
 TDAG = "polytracker.tdag"
 OUTPNG = "output.png"
 RESULTSCSV = "cavities.csv"
 
-def rename_if_exists(src : Path, dst : Path) -> None:
+
+def rename_if_exists(src: Path, dst: Path) -> None:
     if os.path.exists(src):
         rename(src, dst)
 
-def save_results(filename : Path, workdir : Path, output_dir: Path, stats : Dict) -> None:
+
+def save_results(filename: Path, workdir: Path, output_dir: Path, stats: Dict) -> None:
     base = filename.stem
     rename_if_exists(workdir / POLYDB, output_dir / f"{base}.db")
     rename_if_exists(workdir / TDAG, output_dir / f"{base}.tdag")
     rename_if_exists(workdir / OUTPNG, output_dir / f"{base}.png")
     with open(output_dir / f"{base}.meta.json", "w") as fstat:
         json.dump(stats, fstat)
+
+
+@contextmanager
+def create_work_dir(path: Path):
+    try:
+        mkdir(path)
+        yield path.absolute()
+    finally:
+        rmtree(path)
 
 
 # Runs full file cavity detection on draw pdf to png using mutool.
@@ -47,9 +59,8 @@ def file_cavity_detection(file: Path, output_dir: Path, timeout: int) -> str:
     inputdir = os.path.abspath(file.parent)
     stats = {}
     print(f"Processing {filename}")
-    with TemporaryDirectory() as tmpdstr:
-        tmpd = Path(tmpdstr)
 
+    with create_work_dir(Path(output_dir, file.stem)) as tmpd:
         # Run the draw command
         command = [
             "docker",
@@ -65,7 +76,7 @@ def file_cavity_detection(file: Path, output_dir: Path, timeout: int) -> str:
             DOCKER_IMAGE,
             "/usr/bin/bash",
             "-c",
-            f"cd /workdir && DFSAN_OPTIONS=\"strict_data_dependencies=0\" timeout {TIMEOUT} {MUTOOL_PATH} draw -o {OUTPNG} /inputs/{filename}"
+            f'cd /workdir && DFSAN_OPTIONS="strict_data_dependencies=0" timeout {TIMEOUT} {MUTOOL_PATH} draw -o {OUTPNG} /inputs/{filename}',
         ]
 
         stats["draw_command"] = " ".join(command)
@@ -73,16 +84,18 @@ def file_cavity_detection(file: Path, output_dir: Path, timeout: int) -> str:
         return_str = ""
         ret = None
         try:
-            ret = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            #ret = subprocess.run(command)
-            #ret = subprocess.run(command, capture_output=True)
-            #stats["draw_stdout"] = ret.stdout.decode('utf-8')
-            #stats["draw_stderr"] = ret.stderr.decode('utf-8')
+            # ret = subprocess.run(
+            #     command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+            # )
+            # ret = subprocess.run(command)
+            ret = subprocess.run(command, capture_output=True)
+            stats["draw_stdout"] = ret.stdout.decode('utf-8')
+            stats["draw_stderr"] = ret.stderr.decode('utf-8')
             stats["draw_return_code"] = ret.returncode
             if ret.returncode == 124:
                 return_str = f"{filename},-1,-1\n"
                 stats["draw_timeout"] = True
-            if ret.returncode in [125,126,127]: # Failed to run container
+            if ret.returncode in [125, 126, 127]:  # Failed to run container
                 return_str = f"{filename},-3,-3\n"
         finally:
             stats["draw_end"] = time()
@@ -92,26 +105,29 @@ def file_cavity_detection(file: Path, output_dir: Path, timeout: int) -> str:
         if return_str == "":
             command = [
                 "python3",
-                str(SCRIPTDIR / "../polytracker/dumptdag.py"),
+                # str(SCRIPTDIR / "../polytracker/dumptdag.py"),
+                str(SCRIPTDIR / "cavities.py"),
                 str(tmpd / TDAG),
-                f"/inputs/{filename}"
+                f"/inputs/{filename}",
             ]
 
             stats["cavity_compute_command"] = " ".join(command)
             try:
                 stats["cavity_compute_start"] = time()
                 ret = subprocess.run(command, capture_output=True, timeout=timeout)
-                return_str = ret.stdout.decode('utf-8')
+                return_str = ret.stdout.decode("utf-8")
                 stats["cavity_compute_return_code"] = ret.returncode
-                stats["cavity_compute_stderr"] = ret.stderr.decode('utf-8')
-                if ret.returncode in [125,126,127]: # Failed to run container
+                stats["cavity_compute_stderr"] = ret.stderr.decode("utf-8")
+                if ret.returncode in [125, 126, 127]:  # Failed to run container
                     return_str = f"{filename},-4,-4\n"
             except subprocess.TimeoutExpired as e:
                 return_str = f"{filename},-2,-2\n"
                 stats["compute_cavity_timeout"] = True
             finally:
                 stats["cavity_compute_end"] = time()
-                stats["cavity_compute_time"] = stats["cavity_compute_end"] - stats["cavity_compute_start"]
+                stats["cavity_compute_time"] = (
+                    stats["cavity_compute_end"] - stats["cavity_compute_start"]
+                )
 
         save_results(Path(filename), tmpd, output_dir, stats)
         print(f"Finished {filename}")
@@ -121,7 +137,9 @@ def file_cavity_detection(file: Path, output_dir: Path, timeout: int) -> str:
 def execute(output_dir: Path, nworkers: Union[None, int], paths: Iterable[Path]):
     if not os.path.exists(output_dir):
         mkdir(output_dir)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=nworkers) as tpe, open(output_dir / RESULTSCSV, "w") as f:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=nworkers) as tpe, open(
+        output_dir / RESULTSCSV, "w"
+    ) as f:
         futures = []
         for input in paths:
             print(f"Queue {input}")
@@ -139,13 +157,20 @@ def main():
     """
     )
 
-    parser.add_argument("--jobs", "-j", type=int, default=None,
-                        help="Number of jobs to run in parallell")
-    parser.add_argument("--output-dir", "-o", type=Path,
-                        help="Directory where output is stored")
+    parser.add_argument(
+        "--jobs",
+        "-j",
+        type=int,
+        default=None,
+        help="Number of jobs to run in parallell",
+    )
+    parser.add_argument(
+        "--output-dir", "-o", type=Path, help="Directory where output is stored"
+    )
 
-    parser.add_argument("inputs", type=Path, nargs='+',
-                        help="Paths to inputs to mutate")
+    parser.add_argument(
+        "inputs", type=Path, nargs="+", help="Paths to inputs to mutate"
+    )
 
     args = parser.parse_args()
 
