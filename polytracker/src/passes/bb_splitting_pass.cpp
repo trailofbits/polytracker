aee2e8a72969abdfa081f0cc2a8c6274d34351a2
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2022-04-27 13:55:39 +0200

    Refactor BB splitting pass
    This change moves the logging code (used only when DEBUG_INFO is
    defined) into its own function, in order to make the main
    `analyzeBasicBlock` function clearer.

--- polytracker/src/passes/bb_splitting_pass.cpp
+++ polytracker/src/passes/bb_splitting_pass.cpp
@@ -21,6 +21,46 @@ using llvm::Instruction;
 using llvm::isa;
 // using llvm::TerminatorInst;
 
+namespace {
+
+std::optional<std::string> get_function_name(CallInst *call) {
+  if (!call) {
+    return {};
+  }
+
+  if (auto function = call->getCalledFunction()) {
+    if (function->hasName()) {
+      return function->getName().data();
+    }
+    // Note (Carson): Changed getCalledValue() --> Operand()
+    // https://reviews.llvm.org/D78882
+  } else if (auto v = call->getCalledOperand()->stripPointerCasts()) {
+    if (v->hasName()) {
+      return v->getName().data();
+    }
+  }
+  return {};
+}
+
+void log_function(BasicBlock *bb, const std::optional<std::string> &fname) {
+  // Don't bother logging these common functions
+  bool includeFunctionName =
+      (fname != "llvm.dbg.declare" && fname != "llvm.dbg.value" &&
+       fname != "llvm.lifetime.end.p0i8" && fname != "__assert_fail");
+
+  if (!bb || !includeFunctionName) {
+    return;
+  }
+
+  if ((fname || bb->hasName())) {
+    llvm::errs() << " "
+                 << (bb->hasName() ? bb->getName().data() : "Unnamed block");
+    llvm::errs() << " after call to " << (fname.value_or("Unnamed function"))
+                 << "\n";
+  }
+}
+} // namespace
+
 namespace polytracker {
 
 std::vector<BasicBlock *>
@@ -44,31 +84,7 @@ BBSplittingPass::analyzeBasicBlock(BasicBlock &basicBlock) const {
       // We need to split this BB into a new one after the call
       auto bb = next->getParent();
 #ifdef DEBUG_INFO
-      std::string fname;
-      if (auto function = call->getCalledFunction()) {
-        if (function->hasName()) {
-          fname = function->getName().data();
-        }
-        // Note (Carson): Changed getCalledValue() --> Operand()
-        // https://reviews.llvm.org/D78882
-      } else if (auto v = call->getCalledOperand()->stripPointerCasts()) {
-        if (v->hasName()) {
-          fname = v->getName().data();
-        }
-      }
-      // Don't bother logging these common functions, but still split for them
-      bool includeFunctionName =
-          (fname != "llvm.dbg.declare" && fname != "llvm.dbg.value" &&
-           fname != "llvm.lifetime.end.p0i8" && fname != "__assert_fail");
-      if (fname.length() == 0 || bb->hasName() || includeFunctionName) {
-        if (bb->hasName()) {
-          llvm::errs() << " " << bb->getName().data();
-        }
-        if (includeFunctionName) {
-          llvm::errs() << " after call to " << fname;
-        }
-        llvm::errs() << "\n";
-      }
+      log_function(bb, get_function_name(call));
 #endif
       newBBs.push_back(bb->splitBasicBlock(next));
     }
