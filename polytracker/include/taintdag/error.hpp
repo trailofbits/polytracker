b161824b5c004b92fd27a63a12019d245719aa0d
Marek Surovič <marek.surovic@gmail.com>
2022-08-04 16:30:24 +0200

    taintdag: replaces std::cerr for spdlog::error

--- polytracker/include/taintdag/error.hpp
+++ polytracker/include/taintdag/error.hpp
@@ -1,16 +1,25 @@
-#ifndef POLYTRACKER_TAINTDAG_ERROR_H
-#define POLYTRACKER_TAINTDAG_ERROR_H
+/*
+ * Copyright (c) 2022-present, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
 
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+#include <sstream>
 #include <cstdlib>
 
-#include <iostream>
-
 namespace taintdag {
-template <typename... Msgs> void error_exit(Msgs &&... msgs) {
-  std::cerr << "Fatal error. Abort.\n";
-  (std::cerr << ... << msgs) << std::endl;
-  exit(-1);
-}
-} // namespace taintdag
 
-#endif
+template <typename... Msgs> void error_exit(Msgs &&...msgs) {
+  std::stringstream ss;
+  (ss << ... << msgs);
+  spdlog::error(ss.str());
+  std::exit(EXIT_FAILURE);
+}
+
+} // namespace taintdag
