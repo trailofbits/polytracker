156d0d3e74598dd0d72f498e96948c227e75eab3
Marek Surovič <marek.surovic@gmail.com>
2022-08-29 11:50:55 +0200

    tests: identifier rename to clarify purpose

--- tests/test_stdio.py
+++ tests/test_stdio.py
@@ -11,16 +11,16 @@ def set_env_vars(monkeypatch):
 
 
 @pytest.mark.program_trace("test_stdio.cpp")
-def test_tdfile(set_env_vars, program_trace: ProgramTrace):
+def test_stdio(set_env_vars, program_trace: ProgramTrace):
     assert isinstance(program_trace, taint_dag.TDProgramTrace)
     stdout = Path("/dev/stdout")
     stderr = Path("/dev/stderr")
     headers = list(program_trace.tdfile.fd_headers)
-    outputs = list(map(lambda h: h[0], headers))
-    assert len(outputs) == 3
-    assert stdout in outputs
-    assert stderr in outputs
+    paths = list(map(lambda h: h[0], headers))
+    assert len(paths) == 3
+    assert stdout in paths
+    assert stderr in paths
     sinks = list(program_trace.tdfile.sinks)
     assert len(sinks) == 2
-    assert outputs[sinks[0].fdidx] == stdout
-    assert outputs[sinks[1].fdidx] == stderr
+    assert paths[sinks[0].fdidx] == stdout
+    assert paths[sinks[1].fdidx] == stderr
