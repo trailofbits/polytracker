68639bab07eeec58b7d4010a009530e804406037
hbrodin <90325907+hbrodin@users.noreply.github.com>
2022-09-14 10:01:12 +0200

    Reduced namespace pollution.

--- polytracker/src/taint_sources/argv.cpp
+++ polytracker/src/taint_sources/argv.cpp
@@ -4,13 +4,12 @@
 #include "polytracker/polytracker.h"
 #include "taintdag/polytracker.h"
 
-using namespace std::literals;
-
 EARLY_CONSTRUCT_EXTERN_GETTER(taintdag::PolyTracker, polytracker_tdag);
 
 namespace polytracker {
 
 void taint_argv(int argc, char *argv[]) {
+
   // The check could be done in the calling code, for performance reasons.
   // However this function should only ever be invoked once (from main).
   if (!polytracker_taint_argv)
@@ -24,7 +23,7 @@ void taint_argv(int argc, char *argv[]) {
   auto &polyt = get_polytracker_tdag();
 
   for (int i = 0; i < argc; i++) {
-    auto name = "argv["s + std::to_string(i) + "]";
+    auto name = std::string{"argv["} + std::to_string(i) + "]";
     // NOTE(hbrodin): Currently not tainting terminating null char.
     polyt.create_taint_source(
         name, {reinterpret_cast<uint8_t *>(argv[i]), strlen(argv[i])});
