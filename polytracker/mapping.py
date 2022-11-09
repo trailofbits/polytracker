d05246a037ba7c23803af59524fde03fca12726c
kaoudis <kelly.kaoudis@trailofbits.com>
2022-11-09 20:26:30 +0000

    add Henrik's changes to make mypy for 3.7 pass

--- polytracker/mapping.py
+++ polytracker/mapping.py
@@ -4,7 +4,7 @@ This module maps input byte offsets to output byte offsets
 
 from collections import defaultdict
 from pathlib import Path
-from typing import Dict, List, Optional, Set, Tuple, Iterator
+from typing import Dict, Iterator, List, Optional, Set, Tuple
 from tqdm import tqdm
 
 from .plugins import Command
