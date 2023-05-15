64e37091cfd95635a8315c0a0b91534f7a46075a
Lisa Overall <11458176+lisaoverall@users.noreply.github.com>
2023-05-15 08:12:51 -0400

    Harness robust to HTTP requests located anywhere on host file system

--- examples/http/picohttpparser/example_picohttpparser.sh
+++ examples/http/picohttpparser/example_picohttpparser.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ -z "$1" ]]; then
+	echo "Error: no arguments supplied"
+	echo "Usage: ./example_picohttpparser.sh /path/to/raw_http_request"
+	exit 1
+fi
+
 if [[ "$(docker images -q trailofbits/polytracker 2>/dev/null)" == "" ]]; then
 	docker build -t trailofbits/polytracker -f ../../Dockerfile ../../
 fi
@@ -7,4 +13,18 @@ if [[ "$(docker images -q trailofbits/polytracker-demo-http-picohttpparser 2>/de
 	docker build -t trailofbits/polytracker-demo-http-picohttpparser .
 fi
 
-docker run --read-only -ti --rm -e POLYPATH="$1" --mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo-http-picohttpparser:latest /polytracker/examples/http/picohttpparser/example_picohttpparser "$1"
+HOST_PATH=$(realpath "$1")
+HOST_DIR=$(dirname "$HOST_PATH")
+
+# mount the file if it's not already in /workdir
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [[ "$HOST_DIR" == "$SCRIPT_DIR" ]]; then
+	docker run --read-only -ti --rm -e POLYPATH="$1" -e POLYDB="$1.tdag"\
+		--mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo-http-picohttpparser:latest \
+		/polytracker/examples/http/picohttpparser/example_picohttpparser_track "$1"
+else
+	CONTAINER_PATH=/workdir/$(basename "$1")
+	docker run --read-only -ti --rm -v "$HOST_PATH":"$CONTAINER_PATH" -e POLYPATH="$CONTAINER_PATH" -e POLYDB="$CONTAINER_PATH.tdag"\
+		--mount type=bind,source="$(pwd)",target=/workdir trailofbits/polytracker-demo-http-picohttpparser:latest \
+		/polytracker/examples/http/picohttpparser/example_picohttpparser_track "$CONTAINER_PATH"
+fi
