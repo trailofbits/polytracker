e57cdd7409d4cfb2cfaf48389bc3eb20c82344cc
kaoudis <kelly.kaoudis@trailofbits.com>
2023-05-17 17:27:46 +0000

    adds polytracker python package to plain dockerfile so it can be used for analysis if desired

--- examples/analysis/ubet/run.sh
+++ examples/analysis/ubet/run.sh
@@ -5,20 +5,21 @@
 PASSTHROUGH_DOCKER_ARGS=""
 DOCKERFILE="Dockerfile.nitro"
 NO_CACHE="--no-cache"
+IMAGE_NAME="ub-container"
 
 while getopts bp: arg; do
     case "${arg}" in
         b)
-            echo "(Re)building ${DOCKERFILE} container before running..."
-            docker build "${NO_CACHE}" -t ub-container -f "${DOCKERFILE}" .
+            echo "(Re)building ${DOCKERFILE} container and saving as ${IMAGE_NAME} before running..."
+            docker build "${NO_CACHE}" -t "${IMAGE_NAME}" -f "${DOCKERFILE}" .
             ;;
         p)
             PASSTHROUGH_DOCKER_ARGS=${OPTARG}
             ;;
         *)
             echo "== run.sh: run UB Docker container-as-executable =="
-            echo "Optional: -b (builds the ub-container image first from ${DOCKERFILE} with --no-cache)"
-            echo "Required: -p \"passthrough Docker executable arguments to hand to eval script, in quotes\", c.f.:"
+            echo "Optional: -b (builds the ${IMAGE_NAME} image first from ${DOCKERFILE})"
+            echo "Optional: -p \"passthrough Docker executable arguments, in quotes\", c.f.:"
             python3 eval_nitro.py --help
             exit 1
             ;;
@@ -26,6 +27,6 @@ while getopts bp: arg; do
 done
 
 INTERNAL="/polytracker/the_klondike/nitro/build/ubet"
-echo "run.sh: using $(pwd) as the volume attached to container internal location ${INTERNAL}"
+echo "run.sh: using $(pwd) as the volume attached to ${IMAGE_NAME} container internal location ${INTERNAL}"
 
 docker run -it --rm --volume "$(pwd)":"${INTERNAL}" ub-container /usr/bin/bash ${PASSTHROUGH_DOCKER_ARGS}
