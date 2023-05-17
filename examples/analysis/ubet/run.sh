301976bc2a3c6326d62f103632a5aace402fa710
kaoudis <kelly.kaoudis@trailofbits.com>
2023-05-17 16:36:30 +0000

    ensure Dockerfile.nitro builds, add runscript people can use if they want (and so that I can use it when I need it later)

--- examples/analysis/ubet/run.sh
+++ examples/analysis/ubet/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This script and the associated Dockerfiles are known to work on Debian and Ubuntu and have not been tested in other environments.
+
 PASSTHROUGH_DOCKER_ARGS=""
 DOCKERFILE="Dockerfile.nitro"
 NO_CACHE="--no-cache"
@@ -16,16 +18,14 @@ while getopts bp: arg; do
         *)
             echo "== run.sh: run UB Docker container-as-executable =="
             echo "Optional: -b (builds the ub-container image first from ${DOCKERFILE} with --no-cache)"
-            echo "Required: -p \"passthrough Docker executable arguments to hand to eval.py, in quotes\", c.f.:"
+            echo "Required: -p \"passthrough Docker executable arguments to hand to eval script, in quotes\", c.f.:"
             python3 eval_nitro.py --help
             exit 1
             ;;
     esac
 done
 
-#echo "Running last build of ub-container: eval_nitro.py with args '${PASSTHROUGH_DOCKER_ARGS}'..."
-
 INTERNAL="/polytracker/the_klondike/nitro/build/ubet"
-echo "Using $(pwd) as the volume attached to ${INTERNAL}"
+echo "run.sh: using $(pwd) as the volume attached to container internal location ${INTERNAL}"
 
 docker run -it --rm --volume "$(pwd)":"${INTERNAL}" ub-container /usr/bin/bash ${PASSTHROUGH_DOCKER_ARGS}
