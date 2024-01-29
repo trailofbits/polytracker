81596dde733507af837f183570271ffc28c90e16
kaoudis <kelly.kaoudis@trailofbits.com>
2024-01-29 22:37:57 +0000

    fix zlib build pathing for compdiff static build of libtiff

--- examples/analysis/tracing_paper/comparisons/compdiff-asplos-23/build_libtiff.sh
+++ examples/analysis/tracing_paper/comparisons/compdiff-asplos-23/build_libtiff.sh
@@ -12,18 +12,19 @@ mkdir -p "$SRC"
 
 # Step 1, prepare a clean copy of the target sourcecode for build.
 # This step and onward assume this script is run from Dockerfile.libtiff.compdiff.
-cp -r "$PATH_BASE/tiff-4.3.0" "$SRC"
+cp -ra "$PATH_BASE/tiff-4.3.0/." "$SRC"
 
 # Step 2, compile the target
 cd "$SRC" && \
-  build cmake -S . -B build  \
+  cmake -S . -B build  \
     -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_INSTALL_PREFIX="$SRC" \
-    -DZLIB_LIBRARY="$PATH_BASE/zlib-1.3/libz.a" \
+    -DZLIB_LIBRARY="/libtiff/CompDiff/examples/libtiff/zlib-1.3/libz.a" \
     -DCMAKE_C_FLAGS="-O3 -DNDEBUG" \
     -DCMAKE_CXX_FLAGS="-O3 -DNDEBUG" \
     -DCMAKE_EXE_LINKER_FLAGS="-lstdc++" && \
-  cmake --build build -j$((`nproc`+1))
+  cmake --build build -j$((`nproc`+1)) && \
+  cmake --build build --target install
 
 # Step 3, link the target binary out to $BINDIR for afl++
 if [ -z ${DIFF_ID} ]; then
