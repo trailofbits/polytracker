c82c958a28fd940bcf7e5c64881c2abed202ef90
kaoudis <kelly.kaoudis@trailofbits.com>
2024-02-14 21:57:13 +0000

    add a recursive test case to explore how cflog handles recursion

--- tests/test_cf_log.py
+++ tests/test_cf_log.py
@@ -72,3 +72,98 @@ def test_cf_log(instrumented_binary: Path, trace_file: Path):
     assert len(list(cflog)) == len(expected_seq)
     for got, expected in zip(cflog, expected_seq):
         assert got == expected
+
+
+@pytest.mark.program_trace("test_cf_log_recursive.cpp")
+def test_cf_log_recursive(instrumented_binary: Path, trace_file: Path):
+    # Data to write to stdin, one byte at a time
+    stdin_data = "abcdefgh"
+
+    subprocess.run(
+        [str(instrumented_binary)],
+        input=stdin_data.encode("utf-8"),
+        env={
+            "POLYDB": str(trace_file),
+            "POLYTRACKER_STDIN_SOURCE": "1",
+            "POLYTRACKER_LOG_CONTROL_FLOW": "1",
+        },
+    )
+
+    program_trace = polytracker.PolyTrackerTrace.load(trace_file)
+
+    cflog = program_trace.tdfile._get_section(
+        polytracker.taint_dag.TDControlFlowLogSection
+    )
+
+    # The functionid mapping is available next to the built binary
+    with open(instrumented_binary.parent / "functionid.json", "rb") as f:
+        functionid_mapping = list(map(cxxfilt.demangle, json.load(f)))
+
+    # Apply the id to function mappign
+    cflog.function_id_mapping(functionid_mapping)
+    assert cflog.funcmapping is not None
+
+    # once funcmapping is available, the __iter__ method should be functional
+    assert len(list(cflog)) > 0
+
+    expected_seq = [
+        TDEnterFunctionEvent(["main"]),
+        TDEnterFunctionEvent(["main", "f1(unsigned char)"]),
+        TDEnterFunctionEvent(["main", "f1(unsigned char)", "f1(unsigned char)"]),
+        TDEnterFunctionEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f2(unsigned char)"]
+        ),
+        TDLeaveFunctionEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f2(unsigned char)"]
+        ),
+        TDEnterFunctionEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f2(unsigned char)"]
+        ),
+        TDLeaveFunctionEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f2(unsigned char)"]
+        ),
+        TDEnterFunctionEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f1(unsigned char)"]
+        ),
+        TDTaintedControlFlowEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f1(unsigned char)"], 4
+        ),
+        TDEnterFunctionEvent(
+            [
+                "main",
+                "f1(unsigned char)",
+                "f1(unsigned char)",
+                "f1(unsigned char)",
+                "f2(unsigned char)",
+            ]
+        ),
+        TDTaintedControlFlowEvent(
+            [
+                "main",
+                "f1(unsigned char)",
+                "f1(unsigned char)",
+                "f1(unsigned char)",
+                "f2(unsigned char)",
+            ],
+            4,
+        ),
+        TDLeaveFunctionEvent(
+            [
+                "main",
+                "f1(unsigned char)",
+                "f1(unsigned char)",
+                "f1(unsigned char)",
+                "f2(unsigned char)",
+            ]
+        ),
+        TDLeaveFunctionEvent(
+            ["main", "f1(unsigned char)", "f1(unsigned char)", "f1(unsigned char)"]
+        ),
+        TDLeaveFunctionEvent(["main", "f1(unsigned char)", "f1(unsigned char)"]),
+        TDLeaveFunctionEvent(["main", "f1(unsigned char)"]),
+        TDLeaveFunctionEvent(["main"]),
+    ]
+
+    assert len(list(cflog)) == len(expected_seq)
+    for got, expected in zip(cflog, expected_seq):
+        assert got == expected
