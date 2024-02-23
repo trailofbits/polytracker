0a94c6dbe117c9fb8367b86a81d0b617f9309049
kaoudis <kelly.kaoudis@trailofbits.com>
2024-02-23 04:31:44 +0000

    add tqdm so we can see what is happening when the tainted control flow entries are being counted, and how far we get before OOM

--- polytracker/taint_dag.py
+++ polytracker/taint_dag.py
@@ -14,6 +14,8 @@ from typing import (
 
 from argparse import BooleanOptionalAction
 from enum import Enum
+from json import load as jsonload
+from tqdm import tqdm
 from pathlib import Path
 from mmap import mmap, PROT_READ
 from ctypes import (
@@ -201,10 +203,6 @@ class TDControlFlowLogSection:
     Enables enumeration/random access of items.
     """
 
-    # The number of TDTaintedControlFlowEvents (exclusive of function entry
-    # and exit events, which are included for framing in analysis)
-    event_count: int
-
     class Event(Enum):
         """NOTE: MUST correspond to the members in the `ControlFlowLog::EventType`` in `control_flog_log.h`."""
 
@@ -220,22 +218,8 @@ class TDControlFlowLogSection:
         self.mem_reference = mem
         # Call function_id_mapping to set funcmapping before use of cflog.
         self.funcmapping = None
-        self.event_count = 0
 
-    class VarInt:
-        """A dataclass for representing encoded items from the cflog section."""
-
-        value: int
-        index: int
-
-        def __init__(self, value, index):
-            self.value = value
-            self.index = index
-
-        def __repr__(self):
-            return f"value: {self.value}, tdag ending index: {self.index}"
-
-    def _decode_varint(self, starting_index) -> VarInt:
+    def _decode_varint(self, starting_index) -> Tuple[int, int]:
         """Decode a `uint32_t varint` as defined in `control_flow_log.h`. Increment the global cflog index and update the global value upon return. Most varints require only one iteration before `0x80` (varint end) is seen. Called by `__iter__`. NOTE !resets the iteration index!"""
         shift = 0
         decoded_value = 0
@@ -243,7 +227,7 @@ class TDControlFlowLogSection:
             curr = c_uint8.from_buffer_copy(self.mem_reference, j).value
             decoded_value |= (curr & 0x7F) << shift
             if curr & 0x80 == 0:
-                return TDControlFlowLogSection.VarInt(decoded_value, j + 1)
+                return (decoded_value, j + 1)
             shift += 7
         raise Exception(
             "Unable to decode VarInt from TDAG control flow log section; this TDAG may not have been properly written?"
@@ -268,7 +252,9 @@ class TDControlFlowLogSection:
         callstack = []
         event: TDControlFlowLogSection.Event = None
         mem_index = self.cflog_section_start
-        for _ in range(self.cflog_section_start, self.cflog_section_end):
+        for _ in tqdm(
+            range(self.cflog_section_start, self.cflog_section_end), unit="entries"
+        ):
             # do not roll off the end of the section!
             if mem_index + 1 >= self.cflog_section_end:
                 if len(callstack) > 0:
@@ -282,11 +268,11 @@ class TDControlFlowLogSection:
                 mem_index += 1
             else:
                 varint = self._decode_varint(mem_index)
-                mem_index = varint.index
+                mem_index = varint[1]
                 # hack: sometimes function_id otherwise hasnt been set
-                function_id = varint.value
+                function_id = varint[0]
                 if self.funcmapping is not None and len(self.funcmapping) > 0:
-                    function_id = self.funcmapping[varint.value]
+                    function_id = self.funcmapping[function_id]
 
                 if event == TDControlFlowLogSection.Event.ENTER_FUNCTION:
                     event = None
@@ -299,13 +285,25 @@ class TDControlFlowLogSection:
                     )
                 else:
                     varint = self._decode_varint(mem_index)
-                    mem_index = varint.index
+                    mem_index = varint[1]
                     event = None
-                    self.event_count += 1
-                    yield TDTaintedControlFlowEvent(callstack[:], varint.value)
+                    yield TDTaintedControlFlowEvent(callstack[:], varint[0])
 
         # Return back to last function called (replaces exit() etc.)
         yield from TDControlFlowLogSection._align_callstack(-1, callstack)
+
+    @property
+    def event_count(self) -> int:
+        """If this instance's __iter__ has not yet been used, since we only store indices and a memory buffer reference at initialisation time, there are no events to count yet. Returns the total number of events (we use function entries and exits for framing only right now.)"""
+        counter = 0
+        print(
+            f"Iterating over the cflog to cache the count of tainted control flow events, please be patient..."
+        )
+        # todo(kaoudis): a tqdm progress bar doesn't make this any less annoyingly slow for a large section size right now, but could add it
+        for entry in self:
+            if type(entry) == TDTaintedControlFlowEvent:
+                counter += 1
+        return counter
 
 
 class TDSinkSection:
@@ -913,6 +911,14 @@ class TDInfo(Command):
             help="Show info about control flow log trace events.",
         )
 
+        parser.add_argument(
+            "--function_mapping",
+            "-m",
+            default="functionid.json",
+            type=str,
+            help="path to JSON function mapping file (created statically at instrumentation time; needed for cflog event counting)",
+        )
+
     def run(self, args):
         with open(args.POLYTRACKER_TF, "rb") as f:
             tdfile = TDFile(f)
@@ -943,6 +949,5 @@ class TDInfo(Command):
             if args.cflog:
                 cflog = tdfile._get_section(TDControlFlowLogSection)
                 assert isinstance(cflog, TDControlFlowLogSection)
+                cflog.function_id_mapping(jsonload(Path(args.function_mapping).open()))
                 print(f"Number of cflog entries: {cflog.event_count}")
-                # for obj in cflog:
-                #     print(f"{obj}")
