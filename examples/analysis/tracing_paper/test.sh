0285c3e1dcbda7876e19bb6678641b97931f3f5c
kaoudis <kelly.kaoudis@trailofbits.com>
2024-04-19 21:14:39 +0000

    adds python version check to test script

--- examples/analysis/tracing_paper/test.sh
+++ examples/analysis/tracing_paper/test.sh
@@ -11,6 +11,12 @@ set -e
 # The functionid.json from a given binary compilation will always match with
 # all tdags recorded using that binary build!
 
+python_version=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\)\([0-9]\).*/\1\2\3/')
+if [ "$python_version" -lt "310" ]; then
+	echo "We require Python >= 3.10.0; please use a compatible pyenv!"
+	exit 1
+fi
+
 python3 -m pytest -s test/test_analysis.py \
 	--tdag ../ubet/output/Debug.tdag \
 	--tdag2 ../ubet/output/Release.tdag \
