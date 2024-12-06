3e3599eccf576e304521f7ca7aa86196690c1fad
kaoudis <kelly.kaoudis@trailofbits.com>
2024-12-06 20:18:32 +0000

    write the function ID as well as the string table offset of the mangled function symbol into the functions section

--- polytracker/src/taintdag/fnmapping.cpp
+++ polytracker/src/taintdag/fnmapping.cpp
@@ -16,6 +16,7 @@ namespace taintdag {
 namespace {
 
 using index_t = Functions::index_t;
+using offset_t = Function::offset_t;
 
 } // namespace
 
@@ -26,7 +27,6 @@ using index_t = Functions::index_t;
 // In this way, the functions section is a lookup layer for getting names (in their original, mangled format - you can demangle them later with cxxfilt in python) out of the strings table.
 std::optional<index_t> Functions::add_mapping(uint32_t function_id, std::string_view function_name) {
   // Lock `mappings`
-  // std::cout << "BREAK 1" << std::endl;
   std::unique_lock mappings_lock(mappings_mutex);
   // See if we already have a mapping of the function id
   if (auto it{mappings.find(function_id)}; it != mappings.end()) {
@@ -37,7 +37,13 @@ std::optional<index_t> Functions::add_mapping(uint32_t function_id, std::string_
   if (!maybe_name_offset) {
     return {};
   }
-  // Write a `Function` via `construct`
+  // Now write the function ID into the functions section
+  auto maybe_fn_id_ctx{construct((offset_t) function_id)};
+  if (!maybe_fn_id_ctx) {
+    return {};
+  }
+
+  // Finally, write the offset in the string table of the function name into the functions section
   auto name_offset{*maybe_name_offset};
   auto maybe_ctx{construct(name_offset)};
   if (!maybe_ctx) {
