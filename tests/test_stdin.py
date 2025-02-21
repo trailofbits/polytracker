5109db7b99c93d7b13fd4dd6d00432e85029f55a
Kelly Kaoudis <kelly.kaoudis@trailofbits.com>
2025-02-21 16:43:16 +0000

    minor naming cleanup

--- tests/test_stdin.py
+++ tests/test_stdin.py
@@ -13,29 +13,26 @@ from string import printable
 
 _stdin_data = '\n'.join(choice(printable) for _ in range(40)).encode("utf-8")
 
-def _run(instrumented_binary: Path, trace_file: Path, method: str) -> None:
-    """It's important to split out any DRY from the test framework so it's possible to see when an individual test fails."""
-    try:
-        subprocess.run(
-            args=[str(instrumented_binary), method],
-            env={"POLYDB": str(trace_file), "POLYTRACKER_STDIN_SOURCE": "1"},
-            stderr=subprocess.STDOUT,
-            input=_stdin_data,
-            close_fds=False,
-            check=True
-        )
-    except subprocess.CalledProcessError as e:
-        # https://docs.python.org/3/library/subprocess.html#subprocess.CalledProcessError.returncode
-        print(f"Error code: {e.returncode}")
-        print(f"Got back: {e.output}")
-        print(f"stdout: {e.stdout}")
-        print(f"stderr: {e.stderr}")
+def _create_tdag_trace(instrumented_binary: Path, trace_file: Path, method: str) -> None:
+    """Rather than using pytest.mark.parametrize on this setup function, split 
+    out DRY from the test framework so it's easy to see when an individual test
+    fails."""
+    # https://docs.python.org/3/library/subprocess.html#subprocess.CalledProcessError.returncode
+    subprocess.run(
+        args=[str(instrumented_binary), method],
+        env={"POLYDB": str(trace_file), "POLYTRACKER_STDIN_SOURCE": "1"},
+        stderr=subprocess.STDOUT,
+        input=_stdin_data,
+        close_fds=False,
+    ).check_returncode()
 
-def _test_out(program_trace: taint_dag.TDProgramTrace) -> None:
-    """Test the resulting tdag program trace, checking its inputs to make sure 
-    we tainted and tracked every byte of stdin. Offsets must be ordered as they 
-    were read."""
+def _test_trace(trace_file: Path) -> None:
+    """Test the tdag output, checking its inputs to make sure we tainted and 
+    tracked every byte of stdin. Offsets must be ordered as they were read."""
+    
+    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
     assert "/dev/stdin" in [input.path for input in program_trace.inputs]
+    
     expected_offset = 0
     for input_label in program_trace.tdfile.input_labels():
         src_node = program_trace.tdfile.decode_node(input_label)
@@ -43,47 +40,40 @@ def _test_out(program_trace: taint_dag.TDProgramTrace) -> None:
         assert src_node.offset == expected_offset
         assert program_trace.tdfile.fd_headers[src_node.idx][0] == Path("/dev/stdin")
         expected_offset += 1
-
+        
     assert expected_offset == len(_stdin_data)
 
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_read(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "read")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "read")
+    _test_trace(trace_file)
 
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_fread(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "fread")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "fread")
+    _test_trace(trace_file)
     
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_getc(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "getc")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "getc")
+    _test_trace(trace_file)
 
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_getc_unlocked(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "getc_unlocked")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "getc_unlocked")
+    _test_trace(trace_file)
 
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_getchar(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "getchar")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "getchar")
+    _test_trace(trace_file)
 
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_getchar_unlocked(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "getchar_unlocked")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "getchar_unlocked")
+    _test_trace(trace_file)
 
 @pytest.mark.program_trace("test_stdin.cpp")
 def test_stdin_fgetc(instrumented_binary: Path, trace_file: Path):
-    _run(instrumented_binary, trace_file, "fgetc")
-    program_trace: taint_dag.TDProgramTrace = polytracker.PolyTrackerTrace.load(trace_file)
-    _test_out(program_trace)
+    _create_tdag_trace(instrumented_binary, trace_file, "fgetc")
+    _test_trace(trace_file)
