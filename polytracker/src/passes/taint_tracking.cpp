4a4681af887d3373e1f62f4786f41328e0508be0
Kelly Kaoudis <kelly.kaoudis@trailofbits.com>
2025-04-04 11:09:06 +0000

    align extraction index with the size of label_ty

--- polytracker/src/passes/taint_tracking.cpp
+++ polytracker/src/passes/taint_tracking.cpp
@@ -75,7 +75,7 @@ void TaintTrackingPass::insertCondBrLogCall(llvm::Instruction &inst,
   auto dummy_val{val};
 
   if (llvm::isa<llvm::VectorType>(val->getType())) {
-    dummy_val = ir.CreateExtractElement(val, uint64_t(0));
+    dummy_val = ir.CreateExtractElement(val, ir.getInt32(0));
   }
   
   ir.CreateCall(cond_br_log_fn, {ir.CreateSExtOrTrunc(dummy_val, label_ty)});
